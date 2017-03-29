f51f8408e8c036cfbb491dd49b2148baa3b90fdd
David Keymer <david.keymer@stfc.ac.uk>
2017-03-29 17:24:26 +0100

    Added method for removing duplicates in generated lists. Moved printing from class method to calling code.

--- SECI_Config_Analyser/Directory_Operations.py
+++ SECI_Config_Analyser/Directory_Operations.py
@@ -43,19 +43,6 @@ class ReadConfigFiles(object):
 
                 self.comp_filenames.append(filename)
 
-    def analyse_config_files(self):
-        """
-        read XML data in files
-        """
-
-        self.vi_list = []
-
-        print "Config VIs"
-        print "\n".join(self._parse_vis_from_files(self.config_filenames))
-
-        print "Component VIs"
-        print "\n".join(self._parse_vis_from_files(self.comp_filenames))
-
     def _parse_vis_from_files(self, filenames):
         """
         extracts VI names and paths from config files
@@ -64,25 +51,70 @@ class ReadConfigFiles(object):
         """
 
         vis_in_files = []
-        for filename in filenames:
-            filename_and_path = self.full_path + filename
 
+        for filename in filenames:
+
+            filename_and_path = self.full_path + filename
             vis_in_file = self._parse_file(filename_and_path)
             vis_in_files.extend(vis_in_file)
+
         return vis_in_files
 
     def _parse_file(self, filename_and_path):
         """
         reads XML file and creates list of values for "FilePath" tag
-        :param filename_and_path: 
-        :return: 
+        :param filename_and_path: absolute path of file to be parsed
+        :return: vis_in_file: list of  VIs in file
         """
-        print "File being processed: " + filename_and_path
+
+        # print "File being processed: " + filename_and_path
+
         vis_in_file = []
         tree = ET.parse(filename_and_path)
         root = tree.getroot()
         for child in root.iter('FilePath'):
-            vi_path = ET.tostring(child, encoding=None, method="text")
 
+            vi_path = ET.tostring(child, encoding=None, method="text")
             vis_in_file.append(vi_path.strip())
+
         return vis_in_file
+
+    def _remove_duplicates(self, input_list):
+        """
+        remove duplicates from list
+        :param input_list: input list
+        :return: output_list: original list with duplicates removed
+        """
+
+        # maintain list of "encountered" items in dummy list
+        # if item not in this list, add item to it and to output list
+
+        output_list = []
+        encountered = set()
+
+        for item in input_list:
+            item = str.lower(item)
+
+            if item not in encountered:
+                output_list.append(item)
+                encountered.add(item)
+
+        return output_list
+
+    def analyse_config_files(self):
+        """
+        call methods to process files
+        :return: config_vis: list containing absolute paths of VIs within SECI config file
+        :return: comp_vis:   list containing absolute paths of VIs within SECI sub-config file
+        """
+
+        config_vis = self._remove_duplicates(self._parse_vis_from_files(self.config_filenames))
+        comp_vis   = self._remove_duplicates(self._parse_vis_from_files(self.comp_filenames))
+
+        # print "Config VIs"
+        # print "\n".join(config_vis)
+        #
+        # print "Component VIs"
+        # print "\n".join(comp_vis)
+
+        return config_vis, comp_vis
--- SECI_Config_Analyser/test.py
+++ SECI_Config_Analyser/test.py
@@ -1,12 +1,17 @@
 from Directory_Operations import ReadConfigFiles
 
 # create instance of 'ReadConfigFiles' as 'filelist' and supply directory to be read
-filelist = ReadConfigFiles("C:/temp/")
+# filelist = ReadConfigFiles("C:/temp/")
 
-# converts each element in list to string, concatenates with newline character, then prints whole string
-
-# print '\n'.join([str(x) for x in config_filenames])
+filelist = ReadConfigFiles("//ndxvesuvio/c$/Program Files (x86)/STFC ISIS Facility/SECI/Configurations/")
 
 xml_data = filelist.analyse_config_files()
 
 # print xml_data
+
+# concatenates each element in list with newline character, then prints whole string
+
+for item in xml_data:
+
+    print '\n'.join(x for x in item)
+    print "Number of VIs: " + str(len(item))
