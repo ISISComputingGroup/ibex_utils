1d8d1d23ba5ca34c753c8c149d7087222adb0621
John-Holt-Tessella <John.Holt@tessella.com>
2017-11-23 09:37:35 +0000

    exit with error and stop ibex server

--- installation_and_upgrade/install_latest_ibex.bat
+++ installation_and_upgrade/install_latest_ibex.bat
@@ -4,9 +4,8 @@ call "%~dp0\define_latest_genie_python.bat"
 
 set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
 set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
-rem IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
+IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
 
-REM --quiet
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%"  --quiet install_latest
 IF ERRORLEVEL 1 GOTO :ERROR
 
@@ -16,3 +15,4 @@ GOTO :EOF
 
 :ERROR
 popd
+exit /b 2
