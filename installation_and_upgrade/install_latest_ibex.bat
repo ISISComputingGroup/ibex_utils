8b416e0a714133242911c51ef69c8e5da0535b51
John-Holt-Tessella <John.Holt@tessella.com>
2017-11-23 11:48:46 +0000

    Find out which path is being finished on

--- installation_and_upgrade/install_latest_ibex.bat
+++ installation_and_upgrade/install_latest_ibex.bat
@@ -9,10 +9,12 @@ IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%"  --quiet install_latest
 IF ERRORLEVEL 1 GOTO :ERROR
 
+echo "Starting IBEX server"
 start /wait cmd /c "%START_IBEX%"
 popd
 GOTO :EOF
 
 :ERROR
 popd
+echo "Error on Install"
 exit /b 2
