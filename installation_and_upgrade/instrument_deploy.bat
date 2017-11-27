befaa82fe0db8fa408073d70d26e07d6b99ee2ba
Adrian Potter <Adrian.Potter@tessella.com>
2017-11-27 10:53:09 +0000

    Use network version of genie_python. Local is removed as part of deploy

--- installation_and_upgrade/instrument_deploy.bat
+++ installation_and_upgrade/instrument_deploy.bat
@@ -1,10 +1,11 @@
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases\4.0.0"
+call "%~dp0\define_latest_genie_python.bat"
 
 set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
 set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
 IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
 
-C:\Instrument\Apps\Python\python.exe IBEX_upgrade.py --release_dir "%SOURCE%" --confirm_step instrument_deploy
+call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --confirm_step instrument_deploy
 IF ERRORLEVEL 1 EXIT /b %errorlevel%
 
 start /wait cmd /c "%START_IBEX%"
