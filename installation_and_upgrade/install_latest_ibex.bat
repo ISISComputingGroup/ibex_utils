f27d54ded4b5bed6120f442addda4d64f3a75efb
John-Holt-Tessella <John.Holt@tessella.com>
2017-11-27 16:41:54 +0000

    Try adding .bat so that instrument starts and stops correctly

--- installation_and_upgrade/install_latest_ibex.bat
+++ installation_and_upgrade/install_latest_ibex.bat
@@ -2,8 +2,8 @@ REM Install latest version of IBEX
 
 call "%~dp0\define_latest_genie_python.bat"
 
-set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
-set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
+set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server.bat"
+set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server.bat"
 IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
 
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%"  --quiet install_latest
