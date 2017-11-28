6ff66d000432a0417a942bd09a69874d8b695805
John-Holt-Tessella <John.Holt@tessella.com>
2017-11-28 09:11:19 +0000

    Run start and stop in script

--- installation_and_upgrade/install_latest_ibex.bat
+++ installation_and_upgrade/install_latest_ibex.bat
@@ -4,13 +4,13 @@ call "%~dp0\define_latest_genie_python.bat"
 
 set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server.bat"
 set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server.bat"
-IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
+IF EXIST "C:\Instrument\Apps\EPICS" (call "%STOP_IBEX%")
 
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%"  --quiet install_latest
 IF ERRORLEVEL 1 GOTO :ERROR
 
 echo "Starting IBEX server"
-start /wait cmd /c "%START_IBEX%"
+call "%START_IBEX%"
 popd
 GOTO :EOF
 
