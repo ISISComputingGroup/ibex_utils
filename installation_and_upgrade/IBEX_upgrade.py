09a7d12a72b362592494e359d9fef8253260757a
John-Holt-Tessella <John.Holt@tessella.com>
2019-12-18 16:54:54 +0000

    Add genie_python 3 install

--- installation_and_upgrade/IBEX_upgrade.py
+++ installation_and_upgrade/IBEX_upgrade.py
@@ -39,7 +39,8 @@ def _get_latest_release_path(release_dir):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Upgrade the instrument.', formatter_class=argparse.RawTextHelpFormatter)
+    parser = argparse.ArgumentParser(description='Upgrade the instrument.',
+                                     formatter_class=argparse.RawTextHelpFormatter)
 
     parser.add_argument("--release_dir", dest="release_dir", default=None,
                         help="directory from which the client and server should be installed")
@@ -50,13 +51,16 @@ if __name__ == "__main__":
     parser.add_argument("--server_dir", default=None, help="Directory from which IBEX server should be installed")
     parser.add_argument("--client_dir", default=None, help="Directory from which IBEX client should be installed")
     parser.add_argument("--client_e4_dir", default=None, help="Directory from which IBEX E4 client should be installed")
+    parser.add_argument("--genie_python3_dir", default=None,
+                        help="Directory from which genie_python_3 should be installed")
     parser.add_argument("--confirm_step", default=False, action="store_true",
                         help="Confirm each major action before performing it")
     parser.add_argument("--quiet", default=False, action="store_true",
                         help="Do not ask any questions just to the default.")
     parser.add_argument("--kits_icp_dir", default=None, help="Directory of kits/ICP")
 
-    deployment_types = ["{}: {}".format(choice, deployment_types) for choice, (_, deployment_types) in UPGRADE_TYPES.items()]
+    deployment_types = ["{}: {}".format(choice, deployment_types)
+                        for choice, (_, deployment_types) in UPGRADE_TYPES.items()]
     parser.add_argument('deployment_type', choices=UPGRADE_TYPES.keys(),
                         help="What upgrade should be performed. ( {})".format(", \n".join(deployment_types)))
 
@@ -68,16 +72,8 @@ if __name__ == "__main__":
             current_release_dir += "-{}".format(args.release_suffix)
         server_dir = os.path.join(current_release_dir, "EPICS")
         client_dir = os.path.join(current_release_dir, "Client")
-    elif args.server_dir is not None and args.client_dir is not None:
-        server_dir = args.server_dir
-        client_dir = args.client_dir
-    elif args.server_dir is not None or args.client_dir is not None:
-        print("You must specify BOTH the server and client directories.")
-        sys.exit(2)
+        genie_python3_dir = os.path.join(current_release_dir, "genie_python_3")
     elif args.kits_icp_dir is not None:
-        if args.deployment_type not in ['install_latest_incr', 'install_latest']:
-            print("When specifying kits_icp_dir you choose the install latest deployment type.")
-            sys.exit(2)
         if args.deployment_type == 'install_latest_incr':
             epics_build_dir = os.path.join(args.kits_icp_dir, "EPICS", args.server_build_prefix+"_win7_x64")
         else:
@@ -86,26 +82,36 @@ if __name__ == "__main__":
 
         client_build_dir = os.path.join(args.kits_icp_dir, "Client")
         client_dir = _get_latest_directory_path(client_build_dir, "BUILD")
-        if client_e4_dir is None:
-            client_build_dir = os.path.join(args.kits_icp_dir, "Client_E4")
-            client_e4_dir = _get_latest_directory_path(client_build_dir, "BUILD")
+
+        client_e4_build_dir = os.path.join(args.kits_icp_dir, "Client_E4")
+        client_e4_dir = _get_latest_directory_path(client_e4_build_dir, "BUILD")
+
+        genie_python3_build_dir = os.path.join(args.kits_icp_dir, "genie_python_3")
+        genie_python3_dir = _get_latest_directory_path(genie_python3_build_dir, "BUILD")
+
+    elif args.server_dir is not None and args.client_dir is not None and args.genie_python3_dir is not None and \
+            args.client_e4_dir is not None:
+        server_dir = args.server_dir
+        client_dir = args.client_dir
+        client_e4_dir = args.client_e4_dir
+        genie_python3_dir = args.genie_python3_dir
     else:
         print("You must specify either the release directory or kits_icp_dir or "
-              "BOTH the server and client directories.")
+              "ALL of the server, client, client e4 and genie python 3 directories.")
         sys.exit(2)
 
     try:
         prompt = UserPrompt(args.quiet, args.confirm_step)
-        upgrade_instrument = UpgradeInstrument(prompt, server_dir, client_dir, client_e4_dir)
+        upgrade_instrument = UpgradeInstrument(prompt, server_dir, client_dir, client_e4_dir, genie_python3_dir)
         upgrade_function = UPGRADE_TYPES[args.deployment_type][0]
         upgrade_function(upgrade_instrument)
 
     except UserStop:
-        print ("User stopped upgrade")
+        print("User stopped upgrade")
         sys.exit(2)
     except ErrorInTask as error_in_run_ex:
         print("Error in upgrade: {0}".format(error_in_run_ex.message))
         sys.exit(1)
 
-    print ("Finished upgrade")
+    print("Finished upgrade")
     sys.exit(0)
