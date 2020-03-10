60debdd7adb20b4893f506c8696673735c8c5e6e
JamesKingWork <JamesKingWork@ndlt1208.isis.cclrc.ac.uk>
2020-03-10 08:36:13 +0000

    Check for existence of configs rather than machine

--- installation_and_upgrade/ibex_install_utils/install_tasks.py
+++ installation_and_upgrade/ibex_install_utils/install_tasks.py
@@ -629,7 +629,7 @@ class UpgradeTasks(object):
         """
         Update (or at least ask the user to update) the script definitions used by the script generator.
         """
-        if self._get_machine_name() == "NDXEMU":
+        if os.path.exists("C:\\ScriptGeneratorConfigs") or os.path.exists("C:\\ScriptDefinitions"):
             self.prompt.prompt("Update the script definitions for the script generator (likely in C:\\ScriptDefinitions or C:\\ScriptGeneratorConfigs)." + \
                 "Check with the scientists that it is ok to do this." + \
                 "You can do it by git pull, you may need to merge changes made on the instrument.")
