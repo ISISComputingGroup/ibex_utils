cd1e822d6209bb2dd1b2efc29f090542d4aa0c28
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-03-10 11:02:56 +0000

    Don't import inst init

--- installation_and_upgrade/ibex_install_utils/ca_utils.py
+++ installation_and_upgrade/ibex_install_utils/ca_utils.py
@@ -17,7 +17,7 @@ class CaWrapper(object):
         Setting instrument is necessary because genie_python is being run from a network drive so it may not know
         where it is.
         """
-        g.set_instrument(os.getenv("MYPVPREFIX"))
+        g.set_instrument(os.getenv("MYPVPREFIX"), import_instrument_init=False)
 
     def get_local_pv(self, name):
         """
