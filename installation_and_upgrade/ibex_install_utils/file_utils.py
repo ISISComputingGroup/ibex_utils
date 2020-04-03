d9eb95444af8c8be376e45f5fccb3271f2648122
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-04-03 17:12:53 +0100

    Retry delete

--- installation_and_upgrade/ibex_install_utils/file_utils.py
+++ installation_and_upgrade/ibex_install_utils/file_utils.py
@@ -33,7 +33,8 @@ class FileUtils(object):
                     prompt.prompt_and_raise_if_not_yes('Error creating empty dir for robocopy "{}". '
                                                        'Please do this manually'.format(empty_dir))
                 if os.path.isdir(path):
-                    os.system("robocopy \"{}\" \"{}\" /PURGE /NJH /NJS /MT /NP /NFL /NDL /NS /NC /R:1 /LOG:NUL".
+                    for _ in range(0,2):
+                        os.system("robocopy \"{}\" \"{}\" /PURGE /NJH /NJS /MT /NP /NFL /NDL /NS /NC /R:2 /LOG:NUL".
                               format(empty_dir, path))
                 os.rmdir(empty_dir)
                 os.rmdir(path)
