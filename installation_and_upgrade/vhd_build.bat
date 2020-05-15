1bcf6758922f4de5bacf47d0389b77a4d3807abe
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-05-15 21:35:19 +0100

    errorlevel -> %errorlevel%

--- installation_and_upgrade/vhd_build.bat
+++ installation_and_upgrade/vhd_build.bat
@@ -3,4 +3,4 @@ set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 call "%~dp0\define_latest_genie_python.bat"
 
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%" create_vhds --quiet
-IF ERRORLEVEL NEQ 0 EXIT /b %errorlevel%
+IF %ERRORLEVEL% NEQ 0 EXIT /b %errorlevel%
--- installation_and_upgrade/vhd_scheduled_task.bat
+++ installation_and_upgrade/vhd_scheduled_task.bat
@@ -4,7 +4,7 @@ set "SOURCE=C:\Users\Administrator\Documents\fake_release_dir"
 REM Use a locally installed python in C:\users\<username>\Documents\Python as we don't want to execute code directly from
 REM a network share as the admin user.
 call "C:\Users\Administrator\Documents\Python\python.exe" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" mount_vhds --quiet
-IF ERRORLEVEL neq 0 EXIT /b %errorlevel%
+IF %ERRORLEVEL% neq 0 EXIT /b %errorlevel%
 
 call "C:\Users\Administrator\Documents\Python\python.exe" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" dismount_vhds --quiet
-IF ERRORLEVEL neq 0 EXIT /b %errorlevel%
+IF %ERRORLEVEL% neq 0 EXIT /b %errorlevel%
