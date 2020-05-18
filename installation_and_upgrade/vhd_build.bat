2ad30359301e936c8941fedf48580e35b376c8f6
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-05-18 12:41:06 +0100

    Correct syntax

--- installation_and_upgrade/vhd_build.bat
+++ installation_and_upgrade/vhd_build.bat
@@ -3,7 +3,7 @@ set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 call "%~dp0\define_latest_genie_python.bat"
 
 IF EXIST "C:\Instrument\Apps\EPICS\stop_ibex_server.bat" (
-  start /wait cmd /c "C:\Instrument\Apps\EPICS\stop_ibex_server.bat")
+  start /wait cmd /c "C:\Instrument\Apps\EPICS\stop_ibex_server.bat"
 )
 
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%" create_vhds --quiet
