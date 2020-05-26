fe800b33c56ff45f17f1523c78e1e451c4e01b93
RaiBishal <bishal.rai@ndlt1205.isis.cclrc.ac.uk>
2020-05-26 13:56:58 +0100

    Fixed small typo.

--- installation_and_upgrade/ibex_install_utils/install_tasks.py
+++ installation_and_upgrade/ibex_install_utils/install_tasks.py
@@ -1167,7 +1167,7 @@ class UpgradeTasks(object):
             "Please send the following email to your instrument scientists:\n"+\
             "Hello,\n\n We have finished the upgrade of <inst name> to IBEX <X.X.x>."+\
             "The release notes for this are at the following link: https://github.com/ISISComputingGroup/IBEX/wiki/Release-Notes-v<X.X.x>\n\n"+\
-            "Please let us know if you have any querries or find any problems with the upgrade.\n\n"+\
+            "Please let us know if you have any queries or find any problems with the upgrade.\n\n"+\
             "Thank you,\n\n<your name>")
 
     @task("Apply changes in release notes")
