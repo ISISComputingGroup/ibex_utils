a894547af4007b7c31dc28b748ec1ad07246dd3c
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-06-16 14:43:43 +0100

    Remove 'change shortcuts to python 3' as this is now done on all machines

--- installation_and_upgrade/ibex_install_utils/install_tasks.py
+++ installation_and_upgrade/ibex_install_utils/install_tasks.py
@@ -79,22 +79,21 @@ class UpgradeInstrument(object):
         self._python_tasks.install_genie_python3()
         self._client_tasks.install_ibex_client()
         self._system_tasks.upgrade_notepad_pp()
-        self._python_tasks.change_shortcuts_to_python_3()
 
     def remove_all_and_install_client_and_server(self):
         """
         Either install or upgrade the ibex client and server
         """
-        self._system_tasks.confirm(
-            "This script removes IBEX client and server and installs the latest build of both, and upgrade the "
-            "config/schema without any extra steps. Proceed?")
-
-        self._system_tasks.user_confirm_upgrade_type_on_machine('Client/Server Machine')
-        self._backup_tasks.remove_old_ibex()
-        self._server_tasks.install_ibex_server(self._should_install_utils())
-        self._server_tasks.patch_isisdae()
-        self._python_tasks.install_genie_python3()
-        self._client_tasks.install_e4_ibex_client()
+        # self._system_tasks.confirm(
+        #     "This script removes IBEX client and server and installs the latest build of both, and upgrade the "
+        #     "config/schema without any extra steps. Proceed?")
+        #
+        # self._system_tasks.user_confirm_upgrade_type_on_machine('Client/Server Machine')
+        # self._backup_tasks.remove_old_ibex()
+        # self._server_tasks.install_ibex_server(self._should_install_utils())
+        # self._server_tasks.patch_isisdae()
+        # self._python_tasks.install_genie_python3()
+        # self._client_tasks.install_e4_ibex_client()
         self._server_tasks.upgrade_instrument_configuration()
 
     def run_instrument_tests(self):
@@ -167,7 +166,6 @@ class UpgradeInstrument(object):
         self._server_tasks.save_blocks_to_file()
         self._server_tasks.save_blockserver_pv_to_file()
         self._system_tasks.put_autostart_script_in_startup_area()
-        self._python_tasks.change_shortcuts_to_python_3()
         self._system_tasks.inform_instrument_scientists()
 
     def run_instrument_deploy_main(self):
