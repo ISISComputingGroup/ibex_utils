5fb45c0065981d89a806977103b95ee59daf8411
Alistair McGann <alistair.mcgann@tessella.com>
2020-09-17 18:32:32 +0100

    Tidy up

--- galil_ini_parser/sans2d_galils.py
+++ galil_ini_parser/sans2d_galils.py
@@ -36,37 +36,6 @@ def float_to_setting_string(value: Optional[float]) -> Optional[str]:
         return "{:8.6f}".format(value)
 
 
-galil_crates = {}
-
-with open(input_file, 'r') as f:
-    for line in f:
-        # New galil crate if line starts [Gx]
-        if line.startswith("[G"):
-            crate_index = int(line[2])
-            galil_crates[crate_index] = Galil(crate_index)
-        elif "=" in line:
-            galil_crates[crate_index].add_ini_line(line)
-        else:
-            # Not a setting or new galil crate, skip this line
-            pass
-
-reference_galils = {}
-with open(reference_file, 'r') as f:
-    for line in f:
-        # New galil crate if line starts [Gx]
-        if line.startswith("[G"):
-            crate_index = int(line[2])
-            reference_galils[crate_index] = Galil(crate_index)
-        elif "=" in line:
-            reference_galils[crate_index].add_ini_line(line)
-        else:
-            # Not a setting or new galil crate, skip this line
-            pass
-
-# This threshold should be a fraction of the min(encoder or motor res)
-lowerthresh = 1e-5
-
-
 def zero_homeval_zero_offsets(old_homeval: float, old_hlim: float, old_llim: float) -> Dict[str, str]:
     """
     Applies the old homeval as a new offset, and shifts the limits accordingly
@@ -135,13 +104,11 @@ def nonzero_homeval_nonzero_offsets(old_homeval: float,
     # Only write new limits if they aren't infinite
     if abs(old_hlim) != float('inf'):
         new_hlim = old_hlim + difference_in_offsets
-#        axis.set_value(HLIM, "{:8.6f}".format(new_hlim))
     else:
         new_hlim = None
 
     if abs(old_llim) != float('inf'):
         new_llim = old_llim + difference_in_offsets
-#        axis.set_value(LLIM, "{:8.6f}".format(new_llim))
     else:
         new_llim = None
 
@@ -154,8 +121,40 @@ def nonzero_homeval_nonzero_offsets(old_homeval: float,
     return new_settings
 
 
+galil_crates = {}
+
+with open(input_file, 'r') as f:
+    for line in f:
+        # New galil crate if line starts [Gx]
+        if line.startswith("[G"):
+            crate_index = int(line[2])
+            galil_crates[crate_index] = Galil(crate_index)
+        elif "=" in line:
+            galil_crates[crate_index].add_ini_line(line)
+        else:
+            # Not a setting or new galil crate, skip this line
+            pass
+
+reference_galils = {}
+with open(reference_file, 'r') as f:
+    for line in f:
+        # New galil crate if line starts [Gx]
+        if line.startswith("[G"):
+            crate_index = int(line[2])
+            reference_galils[crate_index] = Galil(crate_index)
+        elif "=" in line:
+            reference_galils[crate_index].add_ini_line(line)
+        else:
+            # Not a setting or new galil crate, skip this line
+            pass
+
+# # This threshold should be a fraction of the min(encoder or motor res)
+# lowerthresh = 1e-5
+
+
 for galil in galil_crates.values():
     for axis in galil.axes.values():
+        lowerthresh = get_motor_resolution(axis)
         #print("Galil {} Axis {}".format(galil.crate_index, axis.axis_index))
 
         axis_negated = axis.get_value(NEGATED, bool)
@@ -169,104 +168,20 @@ for galil in galil_crates.values():
 
         if (abs(old_homeval) > lowerthresh) and (abs(old_offset) < lowerthresh) and (abs(old_user_offset) < lowerthresh):
             new_settings = zero_homeval_zero_offsets(old_homeval, old_hlim, old_llim)
+            # new_settings = nonzero_homeval_nonzero_offsets(old_homeval, old_offset, old_user_offset, old_hlim, old_llim)
             for setting, value in new_settings.items():
                 axis.set_value(setting, value)
-            # new_settings = zero_homeval_zero_offsets(old_homeval, old_hlim, old_llim)
-            
-            # # Nonzero homeval, zero offsets
-            # #print("SECI Home Position: {}".format(old_homeval))
-            # new_offset = old_homeval
-            # new_hlim = old_hlim - old_homeval
-            # new_llim = old_llim - old_homeval
-
-            # # Write new values
-            # axis.set_value(OFFSET, "{:8.6f}".format(new_offset))
-            # #print("Offset: {} -> {}".format(old_offset, new_offset))
-
-            # # Only write new limits if they aren't infinite
-            # if abs(old_hlim) != float('inf'):
-            #     axis.set_value(HLIM, "{:8.6f}".format(new_hlim))
-            #     #print("HLIM: {} -> {}".format(old_hlim, new_hlim))
-            # if abs(old_llim) != float('inf'):
-            #     axis.set_value(LLIM, "{:8.6f}".format(new_llim))
-            #     #print("LLIM: {} -> {}".format(old_llim, new_llim))
-
-            # # Write new values
-            # axis.set_value(OFFSET, "{:8.6f}".format(new_offset))
-            # #print("Offset: {} -> {}".format(old_offset, new_offset))
-
-            # # Only write new limits if they aren't infinite
-            # if abs(old_hlim) != float('inf'):
-            #     axis.set_value(HLIM, "{:8.6f}".format(new_hlim))
-            #     #print("HLIM: {} -> {}".format(old_hlim, new_hlim))
-            # if abs(old_llim) != float('inf'):
-            #     axis.set_value(LLIM, "{:8.6f}".format(new_llim))
-            #    # print("LLIM: {} -> {}".format(old_llim, new_llim))
 
         elif (abs(old_homeval) > lowerthresh) and (abs(old_offset + old_user_offset) > lowerthresh):
             new_settings = nonzero_homeval_nonzero_offsets(old_homeval, old_offset, old_user_offset, old_hlim, old_llim)
             for setting, value in new_settings.items():
                 axis.set_value(setting, value)
-            # # Nonzero homeval, nonzero offsets
-            # #print("SECI Home Position: {}".format(old_homeval))
-            # combined_offset = old_offset + old_user_offset
-            # new_offset = old_homeval
-            # new_user_offset = 0.0
-            # difference_in_offsets = combined_offset - old_homeval
-            # # the homeval in motor coords needs to be zero
-            # # => (homeval - any offsets ) == 0
-            # # => combined offsets == homeval
-            # # Only thing which changes is that the limits don't have the offsets applied,
-            # # so need to remember how much we've changed offsets by and change the limits accordingly
-            # new_hlim = old_hlim + difference_in_offsets
-            # new_llim = old_llim + difference_in_offsets
-
-            # # Write new values
-            # axis.set_value(OFFSET, "{:8.6f}".format(new_offset))
-            # #print("Offset: {} -> {}".format(old_offset, new_offset))
-
-            # axis.set_value(USER_OFFSET, "{:8.6f}".format(new_user_offset))
-
-            # # Only write new limits if they aren't infinite
-            # if abs(old_hlim) != float('inf'):
-            #     axis.set_value(HLIM, "{:8.6f}".format(new_hlim))
-            #     #print("HLIM: {} -> {}".format(old_hlim, new_hlim))
-            # if abs(old_llim) != float('inf'):
-            #     axis.set_value(LLIM, "{:8.6f}".format(new_llim))
-            #    # print("LLIM: {} -> {}".format(old_llim, new_llim))
 
         elif (abs(old_homeval) > lowerthresh) and (abs(old_offset + old_user_offset) > lowerthresh) and axis_negated:
             print("Warning! This axis has a negated direction, check its output is sane")
             new_settings = nonzero_homeval_nonzero_offsets(old_homeval, old_offset, old_user_offset, old_hlim, old_llim)
             for setting, value in new_settings.items():
                 axis.set_value(setting, value)
-            # # Nonzero homeval, nonzero offsets
-            # #print("SECI Home Position: {}".format(old_homeval))
-            # combined_offset = old_offset + old_user_offset
-            # new_offset = old_homeval
-            # new_user_offset = 0.0
-            # difference_in_offsets = combined_offset - old_homeval
-            # # the homeval in motor coords needs to be zero
-            # # => (homeval - any offsets ) == 0
-            # # => combined offsets == homeval
-            # # Only thing which changes is that the limits don't have the offsets applied,
-            # # so need to remember how much we've changed offsets by and change the limits accordingly
-            # new_hlim = old_hlim + difference_in_offsets
-            # new_llim = old_llim + difference_in_offsets
-
-            # # Write new values
-            # axis.set_value(OFFSET, "{:8.6f}".format(new_offset))
-            # #print("Offset: {} -> {}".format(old_offset, new_offset))
-
-            # axis.set_value(USER_OFFSET, "{:8.6f}".format(new_user_offset))
-
-            # # Only write new limits if they aren't infinite
-            # if abs(old_hlim) != float('inf'):
-            #     axis.set_value(HLIM, "{:8.6f}".format(new_hlim))
-            #     #print("HLIM: {} -> {}".format(old_hlim, new_hlim))
-            # if abs(old_llim) != float('inf'):
-            #     axis.set_value(LLIM, "{:8.6f}".format(new_llim))
-            #    # print("LLIM: {} -> {}".format(old_llim, new_llim))
 
 # NEED TO MAKE SURE THESE TWO IF STATEMENTS GIVE THE SAME ANSWER WHEN OFFSETS = 0
 # If they do, can delete the first one
@@ -276,7 +191,6 @@ for galil in galil_crates.values():
 for galil in galil_crates.values():
     reference_galil = reference_galils[galil.crate_index]
     for axis in galil.axes.values():
-        print(get_motor_resolution(axis))
         print("Galil {} Axis {}".format(galil.crate_index, axis.axis_index))
         reference_axis = reference_galil.axes[axis.axis_index]
         for setting in axis.settings.keys():
