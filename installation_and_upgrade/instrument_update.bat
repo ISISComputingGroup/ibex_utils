69e85f533879b736eb469e76b617e79a421525f1
Jack Harper <jack.harper@stfc.ac.uk>
2020-09-22 10:50:32 +0100

    call latest_python instead of local python

--- installation_and_upgrade/instrument_update.bat
+++ installation_and_upgrade/instrument_update.bat
@@ -1,10 +1,11 @@
+call "%~dp0\define_latest_genie_python.bat"
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases\4.0.0"
 
 set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
 set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
 IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
 
-C:\Instrument\Apps\Python3\python.exe IBEX_upgrade.py --release_dir "%SOURCE%" --confirm_step instrument_update
+call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --confirm_step instrument_update
 IF ERRORLEVEL 1 exit /b %errorlevel%
 
 start /wait cmd /c "%START_IBEX%"
