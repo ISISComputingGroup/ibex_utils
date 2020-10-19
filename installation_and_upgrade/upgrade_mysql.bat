3c56f7c146614753cbba0c36dd621598ae026539
RaiBishal <bishal.rai@ndlt1205.isis.cclrc.ac.uk>
2020-10-19 23:19:16 +0100

    use python 3 to upgrade mysql

--- installation_and_upgrade/upgrade_mysql.bat
+++ installation_and_upgrade/upgrade_mysql.bat
@@ -5,7 +5,7 @@ set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
 set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
 
 start /wait cmd /c "%STOP_IBEX%"
-
+set "LATEST_PYTHON=C:\Instrument\Apps\Python3\python.exe"
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step force_upgrade_mysql
 IF ERRORLEVEL 1 EXIT /b %errorlevel%
 
