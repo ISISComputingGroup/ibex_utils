abe3db448db2f33a7963f76eadca0c17266a9c60
Tom Willemsen <tomwillemsen1995@gmail.com>
2020-11-03 15:37:07 +0000

    Don't depend on share

--- installation_and_upgrade/windows_10_vhd_deploy.bat
+++ installation_and_upgrade/windows_10_vhd_deploy.bat
@@ -1,28 +1,11 @@
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 set "SUFFIX=%1"
-call "%~dp0\define_latest_genie_python.bat" 3
-IF %errorlevel% neq 0 (
-    echo Cannot define latest python.
-    EXIT /b %errorlevel%
-)
 
-git --version
-IF %errorlevel% neq 0 (
-    echo No installation of Git found on machine. Please download Git from https://git-scm.com/downloads before proceeding.
-    EXIT /b %errorlevel%
-)
+REM since we just installed a VHD, can assume that config_env exists
+call c:\instrument\apps\epics\config_env.bat
 
-set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
-set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
-IF EXIST "C:\Instrument\Apps\EPICS" (start /wait cmd /c "%STOP_IBEX%")
-
-REM Set python as share just for script call
-SETLOCAL
-set "PYTHONDIR=%LATEST_PYTHON_DIR%"
-set "PYTHONHOME=%LATEST_PYTHON_DIR%"
-set "PYTHONPATH=%LATEST_PYTHON_DIR%"
-
-call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step windows_10_vhd_deploy
+REM likewise we can assume that python 3 exists as we just installed a VHD
+call C:\instrument\apps\python3\python.exe "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step windows_10_vhd_deploy
 IF %errorlevel% neq 0 EXIT /b %errorlevel%
 ENDLOCAL
 
