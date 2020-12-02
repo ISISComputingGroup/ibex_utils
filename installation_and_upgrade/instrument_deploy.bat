c90162d5ba51709864b9475ceae6f6505bde2861
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-12-02 17:00:32 +0000

    Fix quoting

--- installation_and_upgrade/instrument_deploy.bat
+++ installation_and_upgrade/instrument_deploy.bat
@@ -13,7 +13,7 @@ set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
 
 IF EXIST "C:\Instrument\Apps\EPICS" (
   REM use python 3 for pre stop as requires genie
-  call "%~dp0\define_latest_genie_python.bat" 3
+  call "%~dp0define_latest_genie_python.bat" 3
   call C:\Instrument\Apps\EPICS\config_env.bat
   SETLOCAL
   set "PYTHONDIR=%LATEST_PYTHON_DIR%"
@@ -26,7 +26,7 @@ IF EXIST "C:\Instrument\Apps\EPICS" (
 )
 
 REM Set python as share just for script call
-call "%~dp0\define_latest_genie_python.bat" 3
+call "%~dp0define_latest_genie_python.bat" 3
 IF %errorlevel% neq 0 goto ERROR
 
 SETLOCAL
@@ -41,10 +41,10 @@ ENDLOCAL
 start /wait cmd /c "%START_IBEX%"
 
 REM python should be installed correctly at this point, so use local python
-set LATEST_PYTHON_DIR="C:\Instrument\Apps\Python3\"
-set PYTHONDIR=%LATEST_PYTHON_DIR%
-set PYTHONHOME=%LATEST_PYTHON_DIR%
-set PYTHONPATH=%LATEST_PYTHON_DIR%
+set "LATEST_PYTHON_DIR=C:\Instrument\Apps\Python3\"
+set "PYTHONDIR=%LATEST_PYTHON_DIR%"
+set "PYTHONHOME=%LATEST_PYTHON_DIR%"
+set "PYTHONPATH=%LATEST_PYTHON_DIR%"
 
 call "%LATEST_PYTHON_DIR%python.exe" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step instrument_deploy_post_start
 :ERROR
