84d061bd578a9336f5c3415d474c6153b66c1611
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2020-12-17 20:32:52 +0000

    Need to use delayed expansion in IF clause

--- installation_and_upgrade/instrument_deploy.bat
+++ installation_and_upgrade/instrument_deploy.bat
@@ -1,4 +1,4 @@
-setlocal
+setlocal EnableDelayedExpansion
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 rem set "RELEASE-SUFFIX="
 
@@ -17,11 +17,11 @@ IF EXIST "C:\Instrument\Apps\EPICS" (
   SETLOCAL
   call "%~dp0define_latest_genie_python.bat" 3
   call C:\Instrument\Apps\EPICS\config_env.bat
-  set "PYTHONDIR=%LATEST_PYTHON_DIR%"
-  set "PYTHONHOME=%LATEST_PYTHON_DIR%"
-  set "PYTHONPATH=%LATEST_PYTHON_DIR%"
-  call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step instrument_deploy_pre_stop
-  IF %errorlevel% neq 0 goto ERROR
+  set "PYTHONDIR=!LATEST_PYTHON_DIR!"
+  set "PYTHONHOME=!LATEST_PYTHON_DIR!"
+  set "PYTHONPATH=!LATEST_PYTHON_DIR!"
+  call "!LATEST_PYTHON!" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step instrument_deploy_pre_stop
+  IF !errorlevel! neq 0 goto ERROR
   start /wait cmd /c "%STOP_IBEX%"
   ENDLOCAL
 )
@@ -43,10 +43,11 @@ start /wait cmd /c "%START_IBEX%"
 
 REM python should be installed correctly at this point, so use local python
 set "LATEST_PYTHON_DIR=C:\Instrument\Apps\Python3\"
+set "LATEST_PYTHON=%LATEST_PYTHON_DIR%python.exe"
 set "PYTHONDIR=%LATEST_PYTHON_DIR%"
 set "PYTHONHOME=%LATEST_PYTHON_DIR%"
 set "PYTHONPATH=%LATEST_PYTHON_DIR%"
 
-call "%LATEST_PYTHON_DIR%python.exe" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step instrument_deploy_post_start
+call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step instrument_deploy_post_start
 :ERROR
 EXIT /b %errorlevel%
