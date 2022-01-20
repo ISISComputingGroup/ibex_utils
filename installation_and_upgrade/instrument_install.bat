9be1b5ae585d3fe754e42b8a5d73adec84bc0553
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-01-20 13:39:37 +0000

    Abort install if run as admin

--- installation_and_upgrade/instrument_install.bat
+++ installation_and_upgrade/instrument_install.bat
@@ -1,4 +1,15 @@
-setlocal
+setlocal EnableDelayedExpansion
+
+REM Prompt user if running console with Administrative privileges.
+net session >nul 2>&1
+if !errorLevel! equ 0 (
+    echo You are in Administrative mode. This may cause file permissioning problems during IBEX setup.
+    CHOICE /C YN /M "Are you sure you want to continue: "
+    IF !errorLevel! equ 1 goto permissiongranted
+    exit /b 1
+)
+:permissiongranted
+
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 set "SUFFIX=%1"
 call "%~dp0\define_latest_genie_python.bat"
