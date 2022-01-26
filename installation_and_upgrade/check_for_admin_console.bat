bcf137b3ac440d40799c569e04e1f8fa99045b75
aaronlong <aaron.long@stfc.ac.uk>
2022-01-26 12:08:34 +0000

    pulled out admin console check to a new bat file

---
+++ installation_and_upgrade/check_for_admin_console.bat
@@ -0,0 +1,10 @@
+REM Prompt user if running console with Administrative privileges.
+net session >nul 2>&1
+if !errorLevel! equ 0 (
+    echo You are in Administrative mode. This may cause file permissioning problems during IBEX setup.
+    CHOICE /C YN /M "Are you sure you want to continue: "
+    IF !errorLevel! equ 1 goto permissiongranted
+    exit /b 1
+)
+:permissiongranted
+exit /b 0
--- installation_and_upgrade/instrument_deploy.bat
+++ installation_and_upgrade/instrument_deploy.bat
@@ -1,14 +1,8 @@
 setlocal EnableDelayedExpansion
 
-REM Prompt user if running console with Administrative privileges.
-net session >nul 2>&1
-if !errorLevel! equ 0 (
-    echo You are in Administrative mode. This may cause file permissioning problems during IBEX setup.
-    CHOICE /C YN /M "Are you sure you want to continue: "
-    IF !errorLevel! equ 1 goto permissiongranted
-    exit /b 1
-)
-:permissiongranted
+REM check if console has Administrative privileges
+call "%~dp0check_for_admin_console.bat"
+IF %errorlevel% neq 0 EXIT /b %errorlevel%
 
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 rem set "RELEASE-SUFFIX="
