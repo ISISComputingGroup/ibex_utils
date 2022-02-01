0b726ea81f7f0b5c1d1f475806b79a4058cc4ddb
JackEAllen <jack.allen@stfc.ac.uk>
2022-02-01 16:15:01 +0000

    Extract Latest Directory and Client Versions into Functions
    Place retrieval of latest release directory and client version into two new functions as part of refactor. Add descriptive comments to script to better understand script behaviour

--- installation_and_upgrade/IBEX_upgrade.py
+++ installation_and_upgrade/IBEX_upgrade.py
@@ -27,6 +27,29 @@ def _get_latest_release_path(release_dir):
     current_release = releases[-1]
     return os.path.join(release_dir, f"{current_release}")
 
+def get_client_version(release_dir):
+    """
+    Get the latest release directory
+    Args:
+        release_dir: directory to search for releases
+    Returns:
+        the latest release directory
+    """
+    current_client_version = _get_latest_release_path(args.release_dir).split("\\")[-1]
+    return current_client_version
+
+def get_latest_release_directory_if_dir(release_dir):
+    """
+    Get the latest release directory
+    Args:
+        release_dir: directory to search for releases
+    Returns:
+        the latest release directory
+    """
+    current_release_dir = os.path.join(args.release_dir, _get_latest_release_path(args.release_dir))  # pylint: disable=line-too-long
+    
+    return current_release_dir
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Upgrade the instrument.',
@@ -34,19 +57,32 @@ if __name__ == "__main__":
 
     parser.add_argument("--release_dir", dest="release_dir", default=None,
                         help="directory from which the client and server should be installed")
+
     parser.add_argument("--release_suffix", dest="release_suffix", default="",
                         help="Suffix for specifying non-standard releases "
                              "(such as those including hot fixes)")
-    parser.add_argument("--server_build_prefix", default="EPICS", help="Prefix for build directory name")  # pylint: disable=line-too-long
-    parser.add_argument("--server_dir", default=None, help="Directory from which IBEX server should be installed")  # pylint: disable=line-too-long
-    parser.add_argument("--client_dir", default=None, help="Directory from which IBEX client should be installed")  # pylint: disable=line-too-long
-    parser.add_argument("--client_e4_dir", default=None, help="Directory from which IBEX E4 client should be installed")  # pylint: disable=line-too-long
+
+    parser.add_argument("--server_build_prefix", default="EPICS",
+    help="Prefix for build directory name")
+
+    parser.add_argument("--server_dir", default=None,
+    help="Directory from which IBEX server should be installed")
+
+    parser.add_argument("--client_dir", default=None,
+    help="Directory from which IBEX client should be installed")
+
+    parser.add_argument("--client_e4_dir", default=None,
+    help="Directory from which IBEX E4 client should be installed")
+
     parser.add_argument("--genie_python3_dir", default=None,
                         help="Directory from which genie_python_3 should be installed")
+
     parser.add_argument("--confirm_step", default=False, action="store_true",
                         help="Confirm each major action before performing it")
+
     parser.add_argument("--quiet", default=False, action="store_true",
                         help="Do not ask any questions just to the default.")
+
     parser.add_argument("--kits_icp_dir", default=None, help="Directory of kits/ICP")
 
     deployment_types = [f"{choice}: {deployment_types}"
@@ -56,11 +92,14 @@ if __name__ == "__main__":
                         .format(", \n".join(deployment_types)))
 
     args = parser.parse_args()
-    client_e4_dir = args.client_e4_dir
-    current_client_version = None
+    client_e4_dir = args.client_e4_dir # The client e4 directory
+    current_client_version = None # The current client version
+
+    # Get the latest release directory, client, client_e4 and genie python versions.
     if args.release_dir is not None:
-        current_release_dir = os.path.join(args.release_dir, _get_latest_release_path(args.release_dir))  # pylint: disable=line-too-long
-        current_client_version = _get_latest_release_path(args.release_dir).split("\\")[-1]
+        current_release_dir = get_latest_release_directory_if_dir(args.release_dir)
+        current_client_version = get_client_version(args.release_dir)
+        # if the release suffix is not empty, then append it to the current release directory
         if args.release_suffix != "":
             current_release_dir += f"-{args.release_suffix}"
         server_dir = os.path.join(current_release_dir, "EPICS")
@@ -68,12 +107,15 @@ if __name__ == "__main__":
         client_e4_dir = client_dir
         genie_python3_dir = os.path.join(current_release_dir, "genie_python_3")
 
+    # Else, if the ICP directory is specified, then use the latest release
     elif args.kits_icp_dir is not None:
+        # If the deployment is equal to the latest release, set the epics build directory to the latest release
         if args.deployment_type == 'install_latest_incr':
             epics_build_dir = os.path.join(args.kits_icp_dir, "EPICS", args.server_build_prefix+"_win7_x64")  # pylint: disable=line-too-long
-        else:
+        else: # Else, set the epics build directory to the latest release.
             epics_build_dir = os.path.join(args.kits_icp_dir, "EPICS", args.server_build_prefix+"_CLEAN_win7_x64")  # pylint: disable=line-too-long
 
+        # Try getting the latest release directory 
         try:
             server_dir = get_latest_directory_path(epics_build_dir, "BUILD-", "EPICS")
 
@@ -89,6 +131,7 @@ if __name__ == "__main__":
             print(error)
             sys.exit(3)
 
+    # Elif the server directory, client, genie_python_3, and client_e4 directories are specified, then use them.
     elif args.server_dir is not None and args.client_dir is not None and \
         args.genie_python3_dir is not None and args.client_e4_dir is not None:
         server_dir = args.server_dir
@@ -100,6 +143,7 @@ if __name__ == "__main__":
               "ALL of the server, client, client e4 and genie python 3 directories.")
         sys.exit(2)
 
+    # Try to upgrade the client.
     try:
         prompt = UserPrompt(args.quiet, args.confirm_step)
         upgrade_instrument = UpgradeInstrument(
