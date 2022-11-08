4c66d51ebef2a940e60fd655c381e4ee5765a68c
Nikola Roev <nikola.roev@stfc.ac.uk>
2022-11-08 10:24:04 +0000

    Updated call_process.

--- installation_and_upgrade/ibex_install_utils/call_process.py
+++ installation_and_upgrade/ibex_install_utils/call_process.py
@@ -6,7 +6,7 @@ import subprocess
 FNULL = open(os.devnull, 'w')
 
 
-def call_process(args: list[str], cwd : StrOrBytesPath=None, capture_output=True, log_args=True) -> int:
+def call_process(args: list[str], cwd : StrOrBytesPath=None) -> int:
     """
         Call a process.
         The 'subprocess' module uses the underlying file descriptor for output,
@@ -17,19 +17,11 @@ def call_process(args: list[str], cwd : StrOrBytesPath=None, capture_output=True
         Args:
             args: arguments to call
             cwd: working directory to call the process in
-            capture_output: whether to capture output from the process
-            log_args: whether to show the full command being executed or just the process
         """
-    if log_args:
-        print(f"Calling: {' '.join(args)}")
-    else:
-        print(f"Calling: {args[0]} (arguments hidden)")
+    print(f"Calling: {' '.join(args)}")
 
-    if capture_output:
-        process = subprocess.Popen(args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
-        for stdout_line in iter(process.stdout.readline, ""):
-            print(f"    {stdout_line}", end="")
-        process.stdout.close()
-        return process.wait()
-    else:
-        return subprocess.call(args=args, cwd=cwd, stdout=FNULL, stderr=FNULL)
+    process = subprocess.Popen(args, cwd=cwd, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, text=True)
+    for stdout_line in iter(process.stdout.readline, ""):
+        print(f"    {stdout_line}", end="")
+    process.stdout.close()
+    return process.wait()
