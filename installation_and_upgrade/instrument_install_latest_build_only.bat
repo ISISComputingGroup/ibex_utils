d25edae279fd93024747547ea4ec289eec99d0bf
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2022-11-21 14:48:16 +0000

    Allow install of 32bit builds

--- installation_and_upgrade/instrument_install_latest_build_only.bat
+++ installation_and_upgrade/instrument_install_latest_build_only.bat
@@ -5,6 +5,10 @@ REM argument 1 is CLEAN or INCR for type of build touse (default: CLEAN)
 REM argument 2 is a server build prefix
 REM normally will use EPICS_win7_x64 or EPICS_CLEAN_win7_x64 depending on incremental/clean
 REM with prefix specified will use {prefix}_win7_x64 and {prefix}_CLEAN_win7_x64 for server install source directory
+
+REM argument 3 can be x86 or x64, defaults to x64 if not specified.
+REM this will change e.g. {prefix}_win7_x64  to {prefix}_win7_x86   as server source directory to use
+
 setlocal EnableDelayedExpansion
 
 set PYTHONUNBUFFERED=TRUE
@@ -28,6 +32,11 @@ if not "%2" == "" (
     @echo Using server build prefix %2
     set SERVER_BUILD_PREFIX=--server_build_prefix "%2"
 )
+
+set SERVER_ARCH=x64
+if not "%3" == "" set SERVER_ARCH=%3
+@echo Using server arch %SERVER_ARCH%
+
 set INSTALL_TYPE=install_latest
 if "%1" == "INCR" (
     set INSTALL_TYPE=install_latest_incr
@@ -38,9 +47,9 @@ if "%1" == "RELEASE" (
     REM set INSTALL_TYPE=instrument_install
     REM set INSTALL_TYPE=training_update
     set INSTALL_TYPE=install_latest
-    "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%RELEASE_SOURCE%" --quiet !INSTALL_TYPE!
+    "%LATEST_PYTHON%" -u "%~dp0IBEX_upgrade.py" --release_dir "%RELEASE_SOURCE%" --server-arch %SERVER_ARCH% --quiet !INSTALL_TYPE!
 ) else (
-    "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%"  %SERVER_BUILD_PREFIX% --quiet !INSTALL_TYPE!
+    "%LATEST_PYTHON%" -u "%~dp0IBEX_upgrade.py" --kits_icp_dir "%KITS_ICP_PATH%"  %SERVER_BUILD_PREFIX% --server-arch %SERVER_ARCH% --quiet !INSTALL_TYPE!
 )
 IF %errorlevel% neq 0 (
     echo Error %errorlevel% returned from IBEX_upgrade script
@@ -50,5 +59,5 @@ IF %errorlevel% neq 0 (
 GOTO :EOF
 
 :ERROR
-echo "Error on Install"
+echo Error on Install
 exit /b 2
