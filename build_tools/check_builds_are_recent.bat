b39d6357da4c9e4adab14d1b910c14f2dc720bf6
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-04-22 23:28:51 +0100

    Use LATEST_PYTHON3 so not killed by stop_ibex_server

--- build_tools/check_builds_are_recent.bat
+++ build_tools/check_builds_are_recent.bat
@@ -1,3 +1,5 @@
+setlocal
 call "%~dp0..\installation_and_upgrade\define_latest_genie_python.bat" 3
 set PYTHONUNBUFFERED=TRUE
-call "%LATEST_PYTHON%" "%~dp0check_builds_are_recent.py"
+REM use LATEST_PYTHON3 so not killed by stop_ibex_server 
+call "%LATEST_PYTHON3%" -u "%~dp0check_builds_are_recent.py"
