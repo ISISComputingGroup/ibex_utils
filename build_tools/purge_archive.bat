07a2010df707bf95f9f7e0294209102874e5f364
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-05-06 01:24:30 +0100

    Install local python

--- build_tools/purge_archive.bat
+++ build_tools/purge_archive.bat
@@ -1,6 +1,14 @@
 setlocal EnableDelayedExpansion
 REM Remove old builds from the archive
 call "%~dp0..\installation_and_upgrade\define_latest_genie_python.bat" 3
+
+if not "%WORKSPACE%" == "" (
+    if exist "%WORKSPACE%\Python3" rd /s /q %WORKSPACE%\Python3
+    call %LATEST_PYTHON_DIR%\genie_python_install.bat %WORKSPACE%\Python3
+    if !errorlevel! neq 0 exit /b 1
+    set "LATEST_PYTHON3=%WORKSPACE%\Python3\python3.exe"
+)
+
 set PYTHONUNBUFFERED=TRUE
 REM use LATEST_PYTHON3 to avoid process being killed 
 "%LATEST_PYTHON3%" -u "%~dp0purge_archive.py"
