e19ed62d18c55c9b98b711213549e1aa349169c9
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-05-06 01:32:50 +0100

    Install local python

--- build_tools/purge_archive.bat
+++ build_tools/purge_archive.bat
@@ -4,7 +4,7 @@ call "%~dp0..\installation_and_upgrade\define_latest_genie_python.bat" 3
 
 if not "%WORKSPACE%" == "" (
     if exist "%WORKSPACE%\Python3" rd /s /q %WORKSPACE%\Python3
-    call %LATEST_PYTHON_DIR%genie_python_install.bat %WORKSPACE%\Python3
+    call %LATEST_PYTHON_DIR%..\genie_python_install.bat %WORKSPACE%\Python3
     if !errorlevel! neq 0 exit /b 1
     set "LATEST_PYTHON3=%WORKSPACE%\Python3\python3.exe"
 )
