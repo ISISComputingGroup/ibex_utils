8eec5faadcc62a9866c83ff405f3f8f9eceb7179
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2023-09-01 21:08:17 +0100

    Check for gamekeeper in addition to existing admin

--- installation_and_upgrade/check_for_admin_console.bat
+++ installation_and_upgrade/check_for_admin_console.bat
@@ -2,10 +2,14 @@ setlocal EnableDelayedExpansion
 REM Prompt user if running console with Administrative privileges.
 net session >nul 2>&1
 if !errorLevel! equ 0 (
-    echo You are in Administrative mode. This may cause file permissioning problems during IBEX setup.
+    echo You are in Administrative mode. This may cause later file permissioning problems during IBEX setup.
     CHOICE /C YN /M "Are you sure you want to continue: "
-    IF !errorLevel! equ 1 goto permissiongranted
-    exit /b 1
+    IF !errorLevel! neq 1 exit /b 1
 )
-:permissiongranted
+if "%USERNAME%" == "gamekeeper" (
+    echo You are using the gamekeeper account. This may cause later file permissioning problems during IBEX setup.
+    CHOICE /C YN /M "Are you sure you want to continue: "
+    IF !errorLevel! neq 1 exit /b 1
+)
+
 exit /b 0
