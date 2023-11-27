214e5e48fd8d3e933a54d1882cb8b610aecf414e
Isaac Hill <isaac.hill@stfc.ac.uk>
2023-11-27 13:28:57 +0000

    release name

--- installation_and_upgrade/ibex_install_utils/tasks/git_tasks.py
+++ installation_and_upgrade/ibex_install_utils/tasks/git_tasks.py
@@ -14,11 +14,18 @@ class GitTasks(BaseTasks):
     @task(f"Swap instrument git branch to release on CONTROL-SVCS")
     def checkout_to_release_branch(self):
         version = open(f"{EPICS_PATH}/VERSION.txt").read().split()[0] 
-        subprocess.call(f"cd {EPICS_PATH} && git remote add origin http://control-svcs.isis.cclrc.ac.uk/gitroot/releases/{version}/EPICS.git", shell=True)
+        subprocess.call(f"cd {EPICS_PATH} && git remote add release http://control-svcs.isis.cclrc.ac.uk/gitroot/releases/{version}/EPICS.git", shell=True)
+        print("Added remote")
         subprocess.call(f"cd {EPICS_PATH} && git add .", shell=True)
-        subprocess.call(f"cd {EPICS_PATH} && git commit -m 'Initial deploy commit'", shell=True)
+        print("Added any changes")
+        subprocess.call(f'cd {EPICS_PATH} && git commit -m "Initial deploy commit"', shell=True)
+        print("Committed any changes")
         subprocess.call(f"cd {EPICS_PATH} && git stash", shell=True)
+        print("Stashed any changes")
         subprocess.call(f"cd {EPICS_PATH} && git checkout -b %COMPUTERNAME%", shell=True)
+        print("Checked out to the new branch")
         subprocess.call(f"cd {EPICS_PATH} && git stash pop", shell=True)
-        subprocess.call(f"cd {EPICS_PATH} && git push -u origin %COMPUTERNAME%", shell=True)
+        print("Popped stash")
+        subprocess.call(f"cd {EPICS_PATH} && git push -u release %COMPUTERNAME%", shell=True)
+        print("Pushed to remote")
     
