db27077acf6e5c4cc9ecd42f473e40b151cd954a
Isaac Hill <isaac.hill@stfc.ac.uk>
2023-12-05 14:43:08 +0000

    initial_commit

--- installation_and_upgrade/ibex_install_utils/tasks/git_tasks.py
+++ installation_and_upgrade/ibex_install_utils/tasks/git_tasks.py
@@ -4,6 +4,7 @@ from ibex_install_utils.task import task
 from ibex_install_utils.tasks import BaseTasks
 from ibex_install_utils.tasks.common_paths import EPICS_PATH
 from genie_python import genie as g
+import re
 
 class GitTasks(BaseTasks):
 
@@ -13,14 +14,37 @@ class GitTasks(BaseTasks):
 
     @task(f"Swap instrument git branch to release on CONTROL-SVCS")
     def checkout_to_release_branch(self):
-        version = open(f"{EPICS_PATH}/VERSION.txt").read().split()[0] 
+        version_pattern = r'^\d+\.\d+\.\d+$'
+
+        with open(f"{EPICS_PATH}/VERSION.txt") as file:
+            version = file.read().split()[0]
+
+        if re.match(version_pattern, version) is None:
+            # assume nightly build so don't want to make new branch/checkout
+            print(f"Version {version} is not a release version, skipping")
+            return
+        
+        try:
         # assumes the alias 'origin' does not exist yet
-        subprocess.call(f"cd {EPICS_PATH} && git remote add origin http://control-svcs.isis.cclrc.ac.uk/gitroot/releases/{version}/EPICS.git", shell=True)
-        print("Added the remote")
-        subprocess.call(f"cd {EPICS_PATH} && git fetch", shell=True)
-        print("Fetched remote")
-        subprocess.call(f"cd {EPICS_PATH} && git checkout -b %COMPUTERNAME%", shell=True)
-        print("Checked out to the new branch")
-        subprocess.call(f"cd {EPICS_PATH} && git push -u origin %COMPUTERNAME%", shell=True)
-        print("Pushed to the remote")
-    
+            subprocess.check_call(f"cd {EPICS_PATH} && git remote add origin http://control-svcs.isis.cclrc.ac.uk/gitroot/releases/{version}/EPICS.git", shell=True)
+            print("Added the remote")
+        except subprocess.CalledProcessError as e:
+            print(f"Error creating remote: {e}")
+        
+        try:
+            subprocess.check_call(f"cd {EPICS_PATH} && git fetch", shell=True)
+            print("Fetched remote")
+        except subprocess.CalledProcessError as e:
+            print(f"Error fetching remote: {e}")
+        
+        try:
+            subprocess.check_call(f"cd {EPICS_PATH} && git checkout -b %COMPUTERNAME%", shell=True)
+            print("Checked out to the new branch")
+        except subprocess.CalledProcessError as e:
+            print(f"Error checking out to new branch: {e}")
+        
+        try:
+            subprocess.check_call(f"cd {EPICS_PATH} && git push -u origin %COMPUTERNAME%", shell=True)
+            print("Pushed to the remote")
+        except subprocess.CalledProcessError as e:
+            print(f"Error pushing to remote: {e}")    
