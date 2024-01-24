c099cdb15d551622838bf265eb73cdb206419b3c
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-01-24 13:18:58 +0000

    Print guidance if share does not exist

--- installation_and_upgrade/instrument_deploy.bat
+++ installation_and_upgrade/instrument_deploy.bat
@@ -8,11 +8,20 @@ set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 set SERVER_ARCH=x64
 if not "%1" == "" set SERVER_ARCH=%1
 
+if not exist "%SOURCE%" (
+    @echo Cannot access network share %SOURCE%
+    @echo If this computer is an instrument, check whether D: drive in explorer is showing
+    @echo as disconnected. If it is, click on it in explorer to reconnect then try this
+    @echo script again. If there are no D: or O: network drives mapped in explorer, then
+    @echo you will need to to provide network credentials for access - see wiki
+    goto ERROR
+)
+
 git --version
 
 if %errorlevel% neq 0 (
     echo No installation of Git found on machine. Please download Git from https://git-scm.com/downloads before proceeding.
-    goto ERROR
+    exit /b %errorlevel%
 )
 
 set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
@@ -22,12 +31,17 @@ IF EXIST "C:\Instrument\Apps\EPICS" (
   REM use python 3 for pre stop as requires genie
   SETLOCAL
   call "%~dp0define_latest_genie_python.bat"
+  IF !errorlevel! neq 0 exit /b !errorlevel!
+  if not exist "!LATEST_PYTHON!" (
+      @echo Cannot find python on network share
+      goto ERROR
+  )
   call C:\Instrument\Apps\EPICS\config_env.bat
   set "PYTHONDIR=%LATEST_PYTHON_DIR%"
   set "PYTHONHOME=%LATEST_PYTHON_DIR%"
   set "PYTHONPATH=%LATEST_PYTHON_DIR%"
   call "!LATEST_PYTHON!" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --server_arch %SERVER_ARCH% --confirm_step instrument_deploy_pre_stop
-  IF !errorlevel! neq 0 goto ERROR
+  IF !errorlevel! neq 0 exit /b !errorlevel!
   start /wait cmd /c "%STOP_IBEX%"
   ENDLOCAL
 )
@@ -62,7 +76,11 @@ if exist "%GALIL_OLD_DIR%\%GALIL_OLD_FILE%" (
 
 REM Set python as share just for script call
 call "%~dp0define_latest_genie_python.bat"
-IF %errorlevel% neq 0 goto ERROR
+IF %errorlevel% neq 0 exit /b %errorlevel%
+if not exist "%LATEST_PYTHON%" (
+    @echo Cannot find python on network share
+    goto ERROR
+)
 
 SETLOCAL
 set "PYTHONDIR=%LATEST_PYTHON_DIR%"
@@ -70,7 +88,7 @@ set "PYTHONHOME=%LATEST_PYTHON_DIR%"
 set "PYTHONPATH=%LATEST_PYTHON_DIR%"
 
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --server_arch %SERVER_ARCH% --confirm_step instrument_deploy_main 
-IF %errorlevel% neq 0 goto ERROR
+IF %errorlevel% neq 0 exit /b %errorlevel%
 ENDLOCAL
 
 start /wait cmd /c "%START_IBEX%"
@@ -84,4 +102,4 @@ set "PYTHONPATH=%LATEST_PYTHON_DIR%"
 
 call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --server_arch %SERVER_ARCH% --confirm_step instrument_deploy_post_start
 :ERROR
-EXIT /b %errorlevel%
+EXIT /b 1
