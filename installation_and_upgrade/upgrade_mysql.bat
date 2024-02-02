a5d2e29aafcc5aa6a5209aab53e95db09d6c71d2
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-02-02 17:57:31 +0000

    Add setlocal

--- installation_and_upgrade/upgrade_mysql.bat
+++ installation_and_upgrade/upgrade_mysql.bat
@@ -1,12 +1,13 @@
+setlocal
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
-call "%~dp0\define_latest_genie_python.bat"
+call "%~dp0define_latest_genie_python.bat"
 
 set "STOP_IBEX=C:\Instrument\Apps\EPICS\stop_ibex_server"
 set "START_IBEX=C:\Instrument\Apps\EPICS\start_ibex_server"
 
 start /wait cmd /c "%STOP_IBEX%"
 
-call "%LATEST_PYTHON%" "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step force_upgrade_mysql
+call "%LATEST_PYTHON%" -u "%~dp0IBEX_upgrade.py" --release_dir "%SOURCE%" --release_suffix "%SUFFIX%" --confirm_step force_upgrade_mysql
 IF %errorlevel% neq 0 EXIT /b %errorlevel%
 
 start /wait cmd /c "%START_IBEX%"
