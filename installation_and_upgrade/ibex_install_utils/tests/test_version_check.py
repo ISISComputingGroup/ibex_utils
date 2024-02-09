6c1cd8610e7d9c4c355df93f243f20bfc8bf5234
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-02-09 19:11:30 +0000

    Remove tests that use local files

--- installation_and_upgrade/ibex_install_utils/tests/test_version_check.py
+++ installation_and_upgrade/ibex_install_utils/tests/test_version_check.py
@@ -21,25 +21,14 @@ class TestVersionCheck(unittest.TestCase):
         with self.assertRaises(AttributeError):
             get_major_minor_patch("java 17.2.4")
 
-    def test_get_file_version(self):
-        example_file = "\\\\isis.cclrc.ac.uk\shares\ISIS_Experiment_Controls_Public\\third_party_installers\latest_versions\Git-2.38.1-64-bit.exe"
-        self.assertEqual(get_file_version(example_file), "2.38.1.1")
+    # def test_get_file_version(self):
+    #     example_file = "\\\\isis.cclrc.ac.uk\shares\ISIS_Experiment_Controls_Public\\third_party_installers\latest_versions\Git-2.38.1-64-bit.exe"
+    #     self.assertEqual(get_file_version(example_file), "2.38.1.1")
 
-    def test_get_msi_property(self):
-        example_file = "\\\\isis.cclrc.ac.uk\shares\ISIS_Experiment_Controls_Public\\third_party_installers\latest_versions\OpenJDK17U-jdk_x64_windows_hotspot_17.0.4.1_1.msi"
-        version = get_msi_property(example_file, MSI_PRODUCT_VERSION_PROPERTY)
-        self.assertEqual(version, "17.0.4.101")
-    
-    # def test_isupper(self):
-    #     self.assertTrue('FOO'.isupper())
-    #     self.assertFalse('Foo'.isupper())
-
-    # def test_split(self):
-    #     s = 'hello world'
-    #     self.assertEqual(s.split(), ['hello', 'world'])
-    #     # check that s.split fails when the separator is not a string
-    #     with self.assertRaises(TypeError):
-    #         s.split(2)
+    # def test_get_msi_property(self):
+    #     example_file = "\\\\isis.cclrc.ac.uk\shares\ISIS_Experiment_Controls_Public\\third_party_installers\latest_versions\OpenJDK17U-jdk_x64_windows_hotspot_17.0.4.1_1.msi"
+    #     version = get_msi_property(example_file, MSI_PRODUCT_VERSION_PROPERTY)
+    #     self.assertEqual(version, "17.0.4.101")
 
 if __name__ == '__main__':
     unittest.main()
