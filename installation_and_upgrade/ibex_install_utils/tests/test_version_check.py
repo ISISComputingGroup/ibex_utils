b48499bf8de43d3694dd6f04982a6b71fe505194
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-02-10 09:38:01 +0000

    Use pytest

--- installation_and_upgrade/ibex_install_utils/tests/test_version_check.py
+++ installation_and_upgrade/ibex_install_utils/tests/test_version_check.py
@@ -1,24 +1,24 @@
-import unittest
+import pytest
 from ibex_install_utils.version_check import *
 
-class TestVersionCheck(unittest.TestCase):
+class TestVersionCheck:
 
     def test_get_major_minor_patch(self):
         # good version patterns
-        self.assertEqual(get_major_minor_patch("17"), "17.0.0")
-        self.assertEqual(get_major_minor_patch("17.0"), "17.0.0")
-        self.assertEqual(get_major_minor_patch("17.23"), "17.23.0")
-        self.assertEqual(get_major_minor_patch("17.0.7.3"), "17.0.7")
-        self.assertEqual(get_major_minor_patch("17.0.0.3"), "17.0.0")
+        assert get_major_minor_patch("17") == "17.0.0"
+        assert get_major_minor_patch("17.0") == "17.0.0"
+        assert get_major_minor_patch("17.23") == "17.23.0"
+        assert get_major_minor_patch("17.0.7.3") == "17.0.7"
+        assert get_major_minor_patch("17.0.0.3") == "17.0.0"
 
         # wrong version patterns
-        with self.assertRaises(AttributeError):
+        with pytest.raises(AttributeError):
             get_major_minor_patch("17.0.0.3.4")
 
-        with self.assertRaises(AttributeError):
+        with pytest.raises(AttributeError):
             get_major_minor_patch("17.")
         
-        with self.assertRaises(AttributeError):
+        with pytest.raises(AttributeError):
             get_major_minor_patch("java 17.2.4")
 
     # def test_get_file_version(self):
@@ -29,6 +29,3 @@ class TestVersionCheck(unittest.TestCase):
     #     example_file = "\\\\isis.cclrc.ac.uk\shares\ISIS_Experiment_Controls_Public\\third_party_installers\latest_versions\OpenJDK17U-jdk_x64_windows_hotspot_17.0.4.1_1.msi"
     #     version = get_msi_property(example_file, MSI_PRODUCT_VERSION_PROPERTY)
     #     self.assertEqual(version, "17.0.4.101")
-
-if __name__ == '__main__':
-    unittest.main()
