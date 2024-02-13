bc741e1435af06a5066f821310be9fe816a96c88
Daniel Maclaren <daniel.maclaren@stfc.ac.uk>
2024-02-13 15:28:52 +0000

    Unit test added backup_checker
    Co-authored-by: esmith1729 <esmith1729@users.noreply.github.com>
    Co-authored-by: Zsolt Kebel <zsoltkebel@users.noreply.github.com>

---
+++ installation_and_upgrade/ibex_install_utils/tests/test_backup_tasks.py
@@ -0,0 +1,38 @@
+#use the following command in an epic terminal 
+#python -m pytest
+#to run the file as a temp fix
+
+import io
+import os
+from unittest.mock import patch, Mock
+from ibex_install_utils.tasks.backup_tasks import BackupTasks
+from ibex_install_utils.tasks import BaseTasks
+from ibex_install_utils.user_prompt import UserPrompt
+
+EPICS_PATH_BACKUP = os.path.join(BaseTasks._get_backup_dir(),'EPICS', 'VERSION.txt')
+PYTHON3_PATH_BACKUP = os.path.join(BaseTasks._get_backup_dir(),'Python3', 'VERSION.txt')
+GUI_PATH_BACKUP = os.path.join(BaseTasks._get_backup_dir(),'Client_E4', 'VERSION.txt') 
+SETTINGS_PATH = os.path.join(BaseTasks._get_backup_dir(), "Settings")
+AUTOSAVE_PATH = os.path.join(BaseTasks._get_backup_dir(), "Autosave")
+UTILS_PATH = os.path.join(BaseTasks._get_backup_dir(), 'EPICS_UTILS')
+
+BACKUP_DIRECTORIES = [
+    EPICS_PATH_BACKUP, PYTHON3_PATH_BACKUP, GUI_PATH_BACKUP, SETTINGS_PATH, AUTOSAVE_PATH, UTILS_PATH
+]
+
+class TestBackupTasks:
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_update_progress_bar(self, mockstdout):
+   
+        BackupTasks('','','','','').update_progress_bar(10, 20)
+        expected_output = '\rProgress: [==========          ] 50% (10/20)'
+        assert mockstdout.getvalue() == expected_output
+    
+    def test_WHEN_verifying_backup_THEN_all_directories_checked(self):
+        os.path.exists = Mock()
+        prompter = UserPrompt(True, False)
+        BackupTasks(prompter,'','','','').backup_checker()
+
+        for dir in BACKUP_DIRECTORIES:
+            os.path.exists.assert_any_call(dir)
+
