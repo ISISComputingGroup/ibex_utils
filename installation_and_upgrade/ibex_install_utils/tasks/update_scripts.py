c7c30f9f1b86b81af3c75c301baf19524e4abe81
ISISBuildServer <freddie.akeroyd@stfc.ac.uk>
2024-02-21 12:16:06 +0000

    Change update script to create branch if doesn't exist, fetch and merge master, and then push to origin

--- installation_and_upgrade/ibex_install_utils/tasks/update_scripts.py
+++ installation_and_upgrade/ibex_install_utils/tasks/update_scripts.py
@@ -3,39 +3,32 @@ import subprocess
 from ibex_install_utils.task import task
 from ibex_install_utils.tasks import BaseTasks
 from ibex_install_utils.tasks.common_paths import SCRIPTS_BASE_DIR
+from ibex_install_utils.user_prompt import UserPrompt
 
 class UpdateScripts(BaseTasks):
     
     @task(f"Update scripts repo by merging master branch into instrument branch?")
     def update_scripts(self):
-        try:
-            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git checkout master", shell=True)
-            print("Checking out master")
-        except subprocess.CalledProcessError as e:
-            print(f"Error checking out master branch: {e}")
         
         try:
-            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git pull", shell=True)
-            print("Fetched remote")
-        except subprocess.CalledProcessError as e:
-            print(f"Error fetching remote: {e}")
-
-        try:
-            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git checkout -b %COMPUTERNAME%", shell=True)
+            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git checkout %COMPUTERNAME% || git checkout -b %COMPUTERNAME%", shell=True)
             print("Checked out to the instrument branch")
         except subprocess.CalledProcessError as e:
-            print(f"Error checking out to new release branch and push: {e}")
-            
-        try: 
-            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git merge master", shell=True)
-            print(f"Merging master into instrument scripts branch")
-        except subprocess.CalledProcessError as e:
-            print(f"Error merging master: {e}")
+            print(f"Error checking out to instrument branch: {e}")
 
         try:
-            # run a git status to rebuild index if needed 
-            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git status", shell=True)
+            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git fetch --all  && git merge master", shell=True)
+            print("Fetching all changes and merging")
         except subprocess.CalledProcessError as e:
-            print(f"Error running git status: {e}")
+            print(f"Error Fetching all changes and merging: {e}")
+
         
-        
+        try:
+            subprocess.check_call(f"cd {SCRIPTS_BASE_DIR} && git push -u origin %COMPUTERNAME%", shell=True)
+            print("Pushing to branch")
+        except subprocess.CalledProcessError as e:
+            print(f"Error pushing to branch: {e}")
+            
+if __name__ == "__main__":
+    prompt = UserPrompt(False,True)
+    UpdateScripts(prompt, "", "", "", "", "").update_scripts()
