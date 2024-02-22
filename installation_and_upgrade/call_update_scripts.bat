c5812e956b346d3477b50b3bf56fe286ca08fa7e
ISISBuildServer <freddie.akeroyd@stfc.ac.uk>
2024-02-22 16:53:23 +0000

    Change path to root for Jenkins pipeline

--- installation_and_upgrade/call_update_scripts.bat
+++ installation_and_upgrade/call_update_scripts.bat
@@ -2,4 +2,5 @@ setlocal
 REM Requires Python3 and python-ssh to be
 
 call
-installation_and_upgrade\pythonwrap.bat .\ installation_and_upgrade\ibex_install_utils\tasks\update_scripts.py
+installation_and_upgrade\pythonwrap.bat .\installation_and_upgrade installation_and_upgrade\ibex_install_utils\tasks\update_scripts.py
+if %errorlevel% neq 0 exit /b %errorlevel%
