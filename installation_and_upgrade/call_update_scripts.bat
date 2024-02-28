ebf96d1fac65223a733fecf84527f7e119ebc085
ISISBuildServer <freddie.akeroyd@stfc.ac.uk>
2024-02-28 11:33:39 +0000

    revert update_scripts.bat to previous version

--- installation_and_upgrade/call_update_scripts.bat
+++ installation_and_upgrade/call_update_scripts.bat
@@ -1,7 +1,5 @@
 setlocal
-call "%~dp0define_latest_genie_python.bat"
-IF %errorlevel% neq 0 EXIT /b %errorlevel%
-set "PYTHONPATH=."
-"%LATEST_PYTHON%" %~dp0ibex_install_utils\tasks\update_scripts.py
-REM c:\instrument\apps\python3\python.exe %~dp0ibex_install_utils\tasks\update_scripts.py
+
+call
+installation_and_upgrade\pythonwrap.bat .\installation_and_upgrade installation_and_upgrade\ibex_install_utils\tasks\update_scripts.py
 if %errorlevel% neq 0 exit /b %errorlevel%
