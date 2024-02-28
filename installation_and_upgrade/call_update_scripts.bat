d2eb0faa0bc96f6b1fde85ecaba3dbbda44d367b
ISISBuildServer <freddie.akeroyd@stfc.ac.uk>
2024-02-28 11:25:55 +0000

    Add /d after cd, change update_scripts.bat structure

--- installation_and_upgrade/call_update_scripts.bat
+++ installation_and_upgrade/call_update_scripts.bat
@@ -1,6 +1,7 @@
 setlocal
-REM Requires Python3 and python-ssh to be
-
-call
-installation_and_upgrade\pythonwrap.bat .\installation_and_upgrade installation_and_upgrade\ibex_install_utils\tasks\update_scripts.py
+call "%~dp0define_latest_genie_python.bat"
+IF %errorlevel% neq 0 EXIT /b %errorlevel%
+set "PYTHONPATH=."
+"%LATEST_PYTHON%" %~dp0ibex_install_utils\tasks\update_scripts.py
+REM c:\instrument\apps\python3\python.exe %~dp0ibex_install_utils\tasks\update_scripts.py
 if %errorlevel% neq 0 exit /b %errorlevel%
