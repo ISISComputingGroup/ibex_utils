8c34653161c21072f22e14b9cc8816f892a81875
esmith1729 <131008854+esmith1729@users.noreply.github.com>
2024-03-11 16:16:47 +0000

    Update run_standalone_backup.bat
    Remove unnecessary comment, change other comment about Python version

--- installation_and_upgrade/run_standalone_backup.bat
+++ installation_and_upgrade/run_standalone_backup.bat
@@ -4,7 +4,6 @@ set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 call "%~dp0\define_latest_genie_python.bat"
 call cd "%~dp0"
 set PYTHONPATH=.
-@REM Needs to be run from installation_and_upgrade directory for modules to work
-@REM Might want to use Python in Instruments/
+@REM Uses Python from the Shares, set as LATEST_PYTHON
 call "%LATEST_PYTHON%" "%~dp0ibex_install_utils\tasks\backup_tasks.py"
 endlocal
