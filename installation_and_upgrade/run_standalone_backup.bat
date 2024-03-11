aa786b88a1282ff92f60b86a5632d33842159607
esmith1729 <131008854+esmith1729@users.noreply.github.com>
2024-03-11 16:14:34 +0000

    Update run_standalone_backup.bat
    Add cd to dir of the bat file, in order to get python modules to work.

--- installation_and_upgrade/run_standalone_backup.bat
+++ installation_and_upgrade/run_standalone_backup.bat
@@ -2,6 +2,7 @@
 setlocal
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 call "%~dp0\define_latest_genie_python.bat"
+call cd "%~dp0"
 set PYTHONPATH=.
 @REM Needs to be run from installation_and_upgrade directory for modules to work
 @REM Might want to use Python in Instruments/
