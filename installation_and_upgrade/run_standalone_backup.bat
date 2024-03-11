64939fca222d6731a0cb75580b5386da925846e9
esmith1729 <131008854+esmith1729@users.noreply.github.com>
2024-03-11 15:54:39 +0000

    Update run_standalone_backup.bat
    Change `python` call to get python from the shares

--- installation_and_upgrade/run_standalone_backup.bat
+++ installation_and_upgrade/run_standalone_backup.bat
@@ -1,6 +1,8 @@
 @ECHO OFF
 setlocal
+set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
+call "%~dp0\define_latest_genie_python.bat"
 set PYTHONPATH=.
 @REM Might want to use Python in Instruments/
-python ibex_install_utils\tasks\backup_tasks.py
+%LATEST_PYTHON% ibex_install_utils\tasks\backup_tasks.py
 endlocal
