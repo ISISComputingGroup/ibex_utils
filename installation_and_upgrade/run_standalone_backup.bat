2599890958b04334452db886f2cada3e34fa25fa
esmith1729 <131008854+esmith1729@users.noreply.github.com>
2024-03-11 16:06:23 +0000

    Update run_standalone_backup.bat
    change set PYTHONPATH=. to cd "%~dp0"

--- installation_and_upgrade/run_standalone_backup.bat
+++ installation_and_upgrade/run_standalone_backup.bat
@@ -2,7 +2,7 @@
 setlocal
 set "SOURCE=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP\Releases"
 call "%~dp0\define_latest_genie_python.bat"
-set PYTHONPATH=.
+cd "%~dp0"
 @REM Might want to use Python in Instruments/
 call "%LATEST_PYTHON%" "%~dp0ibex_install_utils\tasks\backup_tasks.py"
 endlocal
