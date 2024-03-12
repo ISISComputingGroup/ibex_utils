3d0cbb5bf4f51c98e5006bf6d7b5c62175aba644
esmithExperimentControls <evan.smith@stfc.ac.uk>
2024-03-12 09:49:15 +0000

    Change test file to have updated version of progress bar

--- installation_and_upgrade/ibex_install_utils/tests/test_backup_tasks.py
+++ installation_and_upgrade/ibex_install_utils/tests/test_backup_tasks.py
@@ -2,14 +2,17 @@ import io
 import os
 from unittest.mock import patch, Mock
 from ibex_install_utils.tasks.backup_tasks import BackupTasks
+from ibex_install_utils.progress_bar import ProgressBar
 from ibex_install_utils.tasks import BaseTasks
 from ibex_install_utils.user_prompt import UserPrompt
 
 class TestBackupTasks:
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_WHEN_updating_THEN_progress_bar_works(self, mockstdout):
-   
-        BackupTasks('','','','','').update_progress_bar(10, 20)
+        progress_bar = ProgressBar()
+        progress_bar.progress = 10
+        progress_bar.total = 20
+        progress_bar.print()
         expected_output = '\rProgress: [==========          ] 50% (10/20)'
         assert mockstdout.getvalue() == expected_output
     
