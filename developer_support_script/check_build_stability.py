f6c260101f9650e19d9713b49d1c753bbb5c6346
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-06-01 22:14:30 +0100

    Add Win11 system tests to build stability check

--- developer_support_script/check_build_stability.py
+++ developer_support_script/check_build_stability.py
@@ -215,6 +215,7 @@ if __name__ == "__main__":
         "System_Tests_static",
         "System_Tests_win32",
         "System_Tests_galilold",
+        "System_Tests_Win11",
     ]
     SQUISH_JOBS = [
         "System_Tests_Squish"
