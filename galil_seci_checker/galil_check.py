7f1b81bcc8afb84b2a97ba4319f762aea3d32269
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2024-09-24 17:13:32 +0100

    pyright

--- galil_seci_checker/galil_check.py
+++ galil_seci_checker/galil_check.py
@@ -6,8 +6,10 @@
 import argparse
 import configparser
 import os
+import types
 
 from genie_python import genie as g
+from genie_python.genie import PVBaseValue, PVValue
 
 INST = ""
 
@@ -72,7 +74,7 @@ def main() -> None:
             PVPREFIX = f"IN:{INST}:MOT:"
             CONFIGFILE = r"\\NDX{}\c$\LABVIEW MODULES\Drivers\Galil DMC2280\Galil.ini".format(INST)
         else:
-            PVPREFIX = os.getenv("MYPVPREFIX") + "MOT:"
+            PVPREFIX = os.getenv("MYPVPREFIX", default="") + "MOT:"
             CONFIGFILE = r"c:\LABVIEW MODULES\Drivers\Galil DMC2280\Galil.ini"
 
         if args.nocheck:
@@ -103,7 +105,7 @@ def main() -> None:
                 if len(config[g]) > 0:
                     raise Exception("error - non empty DEFAULT")
                 continue
-            doController(config[g], g, skips)
+            do_controller(config[g], g, skips)
     except Exception as ex:
         print(ex)
     if SETFILE is not None:
@@ -114,22 +116,22 @@ def main() -> None:
 
 # note key names are lowercased by the parser e.g. use "motor steps per unit", "kp"
 # however section names and values preserve case
-def doAxis(config: configparser.ConfigParser, galil: str, axis: str, skips: dict) -> None:
-    mn = motorNumber(galil, axis)
-    msteps_per_unit = config.getfloat(axisItem(axis, "motor steps per unit"))
-    esteps_per_unit = config.getfloat(axisItem(axis, "encoder steps per unit"))
+def do_axis(config: configparser.SectionProxy, galil: str, axis: str, skips: dict) -> None:
+    mn = motor_number(galil, axis)
+    msteps_per_unit = config.getfloat(axis_item(axis, "motor steps per unit"))
+    esteps_per_unit = config.getfloat(axis_item(axis, "encoder steps per unit"))
     eres = 1.0 / esteps_per_unit if esteps_per_unit != 0.0 else 0.0
     mres = 1.0 / msteps_per_unit if msteps_per_unit != 0.0 else 0.0
-    speed = config.getfloat(axisItem(axis, "speed"))
+    speed = config.getfloat(axis_item(axis, "speed"))
     velo = speed * mres
-    accl = config.getfloat(axisItem(axis, "acceleration"))
-    dccl = config.getfloat(axisItem(axis, "deceleration"))
+    accl = config.getfloat(axis_item(axis, "acceleration"))
+    dccl = config.getfloat(axis_item(axis, "deceleration"))
     edel_multiplier = 2  # encoder steps for edel
     epics_accl = (
         speed / accl if accl != 0.0 else 0.0
     )  # epics acceleration is time to reach velocity
-    motor_used = config.getboolean(axisItem(axis, "used"))
-    negate_direction = config.getboolean(axisItem(axis, "negate motor direction"))
+    motor_used = config.getboolean(axis_item(axis, "used"))
+    negate_direction = config.getboolean(axis_item(axis, "negate motor direction"))
 
     if not motor_used:
         print(f"INFO: Skipping {mn} as not used in seci")
@@ -142,149 +144,154 @@ def doAxis(config: configparser.ConfigParser, galil: str, axis: str, skips: dict
         "move call",
         "home call",
     ]:  # , 'set points'
-        value = config.get(axisItem(axis, item))
+        value = config.get(axis_item(axis, item))
         if value is not None and value != '""':
             print(f"WARNING: {mn} {item} in seci {galil} axis {axis} is {value}")
 
     # normally like ITA=1\0AKSA=1.313
-    init_bits = config.get(axisItem(axis, "initialisation")).replace('"', "").split(r"\0A")
+    init_bits = config.get(axis_item(axis, "initialisation")).replace('"', "").split(r"\0A")
     for init in init_bits:
         if len(init) > 0 and "=" in init:
             (name, value) = init.split("=")
             init_axis = name[2].lower()
-            init_mn = motorNumber(galil, init_axis)
+            init_mn = motor_number(galil, init_axis)
             item = name[:2]
             if init_axis != axis:
                 print(f"WARNING: initialisation string {name} does not refer to current axis")
             if item == "IT":
-                doValue(f"{init_mn}_ITCSMOOTH_SP", value, f"{init_mn}_ITCSMOOTH_MON")
+                do_value(f"{init_mn}_ITCSMOOTH_SP", value, f"{init_mn}_ITCSMOOTH_MON")
             elif item == "PL":
-                doValue(f"{init_mn}_POLE_SP", value, f"{init_mn}_POLE_MON")
+                do_value(f"{init_mn}_POLE_SP", value, f"{init_mn}_POLE_MON")
             elif item == "KS":
-                doValue(f"{init_mn}_STEPSMOOTH_SP", value, f"{init_mn}_STEPSMOOTH_MON")
+                do_value(f"{init_mn}_STEPSMOOTH_SP", value, f"{init_mn}_STEPSMOOTH_MON")
             elif item == "OF":
-                doValue(f"{init_mn}_BIASVOLTAGE_SP", value, f"{init_mn}_BIASVOLTAGE_MON")
+                do_value(f"{init_mn}_BIASVOLTAGE_SP", value, f"{init_mn}_BIASVOLTAGE_MON")
             elif item == "OE":
-                doValue(f"{init_mn}_OFFONERR_CMD", value, f"{init_mn}_OFFONERR_STATUS")
+                do_value(f"{init_mn}_OFFONERR_CMD", value, f"{init_mn}_OFFONERR_STATUS")
             elif item == "ER":
-                doValue(f"{init_mn}_ERRLIMIT_SP", value, f"{init_mn}_ERRLIMIT_MON")
+                do_value(f"{init_mn}_ERRLIMIT_SP", value, f"{init_mn}_ERRLIMIT_MON")
             else:
-                doValue(f"{init_mn}_{item}_SP", value, f"{init_mn}_{item}_MONITOR")
+                do_value(f"{init_mn}_{item}_SP", value, f"{init_mn}_{item}_MONITOR")
     if not NOCHECK:
         for item in ["PREM", "POST", "INIT"]:
             value = g.get_pv(f"{PVPREFIX}{mn}.{item}")
             if value is not None and value != "":
                 print(f'INFO: {PVPREFIX}{mn}.{item} is "{value}"')
 
-    doValue(f"{mn}.EGU", config.get(axisItem(axis, "unit label")))
-    doValue(f"{mn}.DESC", config.get(axisItem(axis, "motor name")))
-    ueip = config.getboolean(axisItem(axis, "encoder present"))
+    do_value(f"{mn}.EGU", config.get(axis_item(axis, "unit label")))
+    do_value(f"{mn}.DESC", config.get(axis_item(axis, "motor name")))
+    ueip = config.getboolean(axis_item(axis, "encoder present"))
     if not skips["ueip"]:
-        doValue(f"{mn}.UEIP", "Yes" if ueip else "No")
+        do_value(f"{mn}.UEIP", "Yes" if ueip else "No")
     for item in ["k1", "k2", "k3", "zp", "zn", "tl", "ct"]:
         uitem = item.upper()
-        doValue(f"{mn}_{uitem}_SP", config.getfloat(axisItem(axis, item)), f"{mn}_{uitem}_MONITOR")
-    doValue(f"{mn}.PCOF", config.getfloat(axisItem(axis, "kp")) / 1023.875)
-    doValue(
-        f"{mn}.ICOF", config.getfloat(axisItem(axis, "ki")) / 2047.875
+        do_value(
+            f"{mn}_{uitem}_SP", config.getfloat(axis_item(axis, item)), f"{mn}_{uitem}_MONITOR"
+        )
+    do_value(f"{mn}.PCOF", config.getfloat(axis_item(axis, "kp")) / 1023.875)
+    do_value(
+        f"{mn}.ICOF", config.getfloat(axis_item(axis, "ki")) / 2047.875
     )  # note: different scaling would be needed for galil 4000
-    doValue(f"{mn}.DCOF", config.getfloat(axisItem(axis, "kd")) / 4095.875)
-    doValue(f"{mn}.MRES", mres)
-    doValue(f"{mn}.ERES", eres)
+    do_value(f"{mn}.DCOF", config.getfloat(axis_item(axis, "kd")) / 4095.875)
+    do_value(f"{mn}.MRES", mres)
+    do_value(f"{mn}.ERES", eres)
     if not skips["edel"]:
-        doValue(f"{mn}_EDEL_SP", edel_multiplier * eres, f"{mn}_EDEL_MON")  # old galil
-        doValue(f"{mn}_ENC_TOLERANCE_SP", edel_multiplier, f"{mn}_ENC_TOLERANCE_MON")  # new galil
-    hspeed = config.getfloat(axisItem(axis, "home speed"))
+        do_value(f"{mn}_EDEL_SP", edel_multiplier * eres, f"{mn}_EDEL_MON")  # old galil
+        do_value(f"{mn}_ENC_TOLERANCE_SP", edel_multiplier, f"{mn}_ENC_TOLERANCE_MON")  # new galil
+    hspeed = config.getfloat(axis_item(axis, "home speed"))
     if not skips["velocity"]:
-        doValue(f"{mn}.VELO", velo)
-        doValue(f"{mn}.VMAX", velo)
-        doValue(f"{mn}.JVEL", velo)
-        doValue(f"{mn}.HVEL", hspeed * mres)
-    doValue(f"{mn}.RDBD", config.getfloat(axisItem(axis, "positional accuracy")))
+        do_value(f"{mn}.VELO", velo)
+        do_value(f"{mn}.VMAX", velo)
+        do_value(f"{mn}.JVEL", velo)
+        do_value(f"{mn}.HVEL", hspeed * mres)
+    do_value(f"{mn}.RDBD", config.getfloat(axis_item(axis, "positional accuracy")))
     if not skips["spdb"]:
-        doValue(f"{mn}.SPDB", config.getfloat(axisItem(axis, "set-point deadband")))
+        do_value(f"{mn}.SPDB", config.getfloat(axis_item(axis, "set-point deadband")))
     if negate_direction:
         print(f"INFO: {mn} has seci negated direction")
     if not skips["direction"]:
-        doValue(f"{mn}.DIR", "Neg" if negate_direction else "Pos")
+        do_value(f"{mn}.DIR", "Neg" if negate_direction else "Pos")
 
     if not skips["acceleration"]:
-        doValue(f"{mn}.ACCL", epics_accl)
-        doValue(f"{mn}.JAR", epics_accl)
+        do_value(f"{mn}.ACCL", epics_accl)
+        do_value(f"{mn}.JAR", epics_accl)
 
-    doValue(f"{mn}.RMOD", "Default")
-    # doValue(f"{mn}_HOMEVAL_SP", 0)
-    doValue(f"{mn}_WLP_CMD", "Off")
+    do_value(f"{mn}.RMOD", "Default")
+    # do_value(f"{mn}_HOMEVAL_SP", 0)
+    do_value(f"{mn}_WLP_CMD", "Off")
     if not skips["onoff"]:
-        doValue(f"{mn}_OFFDELAY_SP", 2)
-        doValue(f"{mn}_ONDELAY_SP", 0)
-    doValue(f"{mn}_JAH_CMD", "No")
+        do_value(f"{mn}_OFFDELAY_SP", 2)
+        do_value(f"{mn}_ONDELAY_SP", 0)
+    do_value(f"{mn}_JAH_CMD", "No")
 
-    doValue(
+    do_value(
         f"{mn}_MTRTYPE_CMD",
-        MOTOR_TYPES[config.getint(axisItem(axis, "motor type"))],
+        MOTOR_TYPES[config.getint(axis_item(axis, "motor type"))],
         f"{mn}_MTRTYPE_STATUS",
     )
-    doValue(
+    do_value(
         f"{mn}_MENCTYPE_CMD",
-        ENCODER_TYPES[config.getint(axisItem(axis, "encoder type"))],
+        ENCODER_TYPES[config.getint(axis_item(axis, "encoder type"))],
         f"{mn}_MENCTYPE_STATUS",
     )
 
-    de_energise = config.getboolean(axisItem(axis, "de-energise"))
+    de_energise = config.getboolean(axis_item(axis, "de-energise"))
     if not skips["onoff"]:
-        doValue(f"{mn}_AUTOONOFF_CMD", "On" if de_energise else "Off", f"{mn}_AUTOONOFF_STATUS")
-        doValue(f"{mn}_ON_CMD", "Off" if de_energise else "On", f"{mn}_ON_STATUS")
+        do_value(f"{mn}_AUTOONOFF_CMD", "On" if de_energise else "Off", f"{mn}_AUTOONOFF_STATUS")
+        do_value(f"{mn}_ON_CMD", "Off" if de_energise else "On", f"{mn}_ON_STATUS")
 
-    af = config.getboolean(axisItem(axis, "analog feedback"))
-    doValue(f"{mn}_AF_SP", af, f"{mn}_AF_MONITOR")
+    af = config.getboolean(axis_item(axis, "analog feedback"))
+    do_value(f"{mn}_AF_SP", af, f"{mn}_AF_MONITOR")
 
-    doValue(f"{mn}.RTRY", 10 if ueip and config.getboolean(axisItem(axis, "correct motion")) else 0)
-
-    if not skips["enable"]:
-        doValue(f"{mn}_able", "Enable" if motor_used else "Disable")
-
-    print(
-        f"INFO: {mn} seci home method: ", HOME_METHODS[config.getint(axisItem(axis, "home method"))]
+    do_value(
+        f"{mn}.RTRY", 10 if ueip and config.getboolean(axis_item(axis, "correct motion")) else 0
     )
 
-    apply_home = config.getboolean(axisItem(axis, "apply home position"))
-    seci_home_pos = config.getfloat(axisItem(axis, "home position"), 0.0)
-    seci_home_offset = config.getfloat(axisItem(axis, "home offset"), 0.0)
-    seci_offset = config.getfloat(axisItem(axis, "offset"), 0.0)
-    seci_user_offset = config.getfloat(axisItem(axis, "user offset"), 0.0)
+    if not skips["enable"]:
+        do_value(f"{mn}_able", "Enable" if motor_used else "Disable")
+
+    print(
+        f"INFO: {mn} seci home method: ",
+        HOME_METHODS[config.getint(axis_item(axis, "home method"))],
+    )
+
+    apply_home = config.getboolean(axis_item(axis, "apply home position"))
+    seci_home_pos = config.getfloat(axis_item(axis, "home position"), 0.0)
+    seci_home_offset = config.getfloat(axis_item(axis, "home offset"), 0.0)
+    seci_offset = config.getfloat(axis_item(axis, "offset"), 0.0)
+    seci_user_offset = config.getfloat(axis_item(axis, "user offset"), 0.0)
     print(
         f"INFO: {mn} seci home position {seci_home_pos} (apply: {apply_home}) "
         f"home offset {seci_home_offset} offset {seci_offset} user offset {seci_user_offset}"
     )
     if not skips["offset"]:
         if apply_home:
-            doValue(f"{mn}.OFF", seci_home_pos - seci_offset - seci_user_offset)
+            do_value(f"{mn}.OFF", seci_home_pos - seci_offset - seci_user_offset)
         else:
-            doValue(f"{mn}.OFF", -seci_offset - seci_user_offset)
+            do_value(f"{mn}.OFF", -seci_offset - seci_user_offset)
 
     eguaft = hspeed * hspeed / dccl / 2.0 / msteps_per_unit
     if not skips["eguaftlimit"]:
-        doValue(f"{mn}_EGUAFTLIMIT_SP", eguaft, f"{mn}_EGUAFTLIMIT_MON")
+        do_value(f"{mn}_EGUAFTLIMIT_SP", eguaft, f"{mn}_EGUAFTLIMIT_MON")
 
-    cp = config.getint(axisItem(axis, "CP"))
+    cp = config.getint(axis_item(axis, "CP"))
     if de_energise:
         cp = 0 if cp > 0 else -1
     else:
         cp = -1
-    doValue(f"{mn}_CP_SP", cp, f"{mn}_CP_MONITOR")
+    do_value(f"{mn}_CP_SP", cp, f"{mn}_CP_MONITOR")
 
-    fbl = config.getfloat(axisItem(axis, "forward backlash"))
-    bbl = config.getfloat(axisItem(axis, "backward backlash"))
-    stall_error = config.getfloat(axisItem(axis, "stall allowed error"))
-    if config.getboolean(axisItem(axis, "stall enable")):
-        stall_error = config.getfloat(axisItem(axis, "stall allowed error"))
+    fbl = config.getfloat(axis_item(axis, "forward backlash"))
+    bbl = config.getfloat(axis_item(axis, "backward backlash"))
+    stall_error = config.getfloat(axis_item(axis, "stall allowed error"))
+    if config.getboolean(axis_item(axis, "stall enable")):
+        stall_error = config.getfloat(axis_item(axis, "stall allowed error"))
     else:
         stall_error = max(abs(fbl), abs(bbl))
     stall_time = msteps_per_unit * abs(stall_error) / speed
-    doValue(f"{mn}_ESTALLTIME_SP", stall_time if stall_time > 1.2 else 1.2, f"{mn}_ESTALLTIME_MON")
+    do_value(f"{mn}_ESTALLTIME_SP", stall_time if stall_time > 1.2 else 1.2, f"{mn}_ESTALLTIME_MON")
 
-    if config.getboolean(axisItem(axis, "rerun home")):
+    if config.getboolean(axis_item(axis, "rerun home")):
         print(f"INFO: {mn} rerun home enabled in seci")
 
     backlash = 0
@@ -304,16 +311,16 @@ def doAxis(config: configparser.ConfigParser, galil: str, axis: str, skips: dict
             "then BVEL or BACC mismatches are not critical"
         )
     if not skips["backlash"]:
-        doValue(f"{mn}.BVEL", velo / 10.0)
-        doValue(f"{mn}.BACC", epics_accl / 10.0)
-        doValue(f"{mn}.BDST", -backlash)
+        do_value(f"{mn}.BVEL", velo / 10.0)
+        do_value(f"{mn}.BACC", epics_accl / 10.0)
+        do_value(f"{mn}.BDST", -backlash)
 
-    smax = config.getfloat(axisItem(axis, "soft max"))
+    smax = config.getfloat(axis_item(axis, "soft max"))
     smax = smax if smax != float("inf") else 0
-    smin = config.getfloat(axisItem(axis, "soft min"))
+    smin = config.getfloat(axis_item(axis, "soft min"))
     smin = smin if smin != float("-inf") else 0
-    umax = config.getfloat(axisItem(axis, "user max"))
-    umin = config.getfloat(axisItem(axis, "user min"))
+    umax = config.getfloat(axis_item(axis, "user max"))
+    umin = config.getfloat(axis_item(axis, "user min"))
     if umax is not None and umax != float("inf"):
         print(
             f"user max defined as {umax} with soft max as {smax}, "
@@ -329,8 +336,8 @@ def doAxis(config: configparser.ConfigParser, galil: str, axis: str, skips: dict
     if smin > smax:
         print(f"ERROR: SECI min limit {smin} > SECI max limit {smax}, will not adjust limits")
     elif not skips["limits"]:
-        doValue(f"{mn}.HLM", smax)
-        doValue(f"{mn}.LLM", smin)
+        do_value(f"{mn}.HLM", smax)
+        do_value(f"{mn}.LLM", smin)
 
 
 # soft min   soft max        home offset     position ref    position
@@ -338,11 +345,11 @@ def doAxis(config: configparser.ConfigParser, galil: str, axis: str, skips: dict
 # Home - Move Pos        Home - Move After      Axis A Set Points = ""
 # Axis A Coerce Set Point = FALSE      Exclude Global Move       Stall Enable
 # Stall Allowed Error
-def axisItem(axis: str, name: str) -> str:
+def axis_item(axis: str, name: str) -> str:
     return f"axis {axis} {name}"
 
 
-def doValue(set_pv: str, value: str, read_pv: str | type.NoneType = None) -> None:
+def do_value(set_pv: str, value: str | float, read_pv: str | types.NoneType = None) -> None:
     prefixed_read_pv = "{}{}".format(PVPREFIX, read_pv)
     prefixed_set_pv = "{}{}".format(PVPREFIX, set_pv)
     if isinstance(value, str):
@@ -354,7 +361,7 @@ def doValue(set_pv: str, value: str, read_pv: str | type.NoneType = None) -> Non
         return
     current_sp = g.get_pv(prefixed_set_pv)
     sp_ok = True
-    if not compareValues(current_sp, value):
+    if not compare_values(current_sp, value):
         print(
             '{} SP differs: current "{}" != expected "{}"'.format(
                 prefixed_set_pv, current_sp, value
@@ -368,7 +375,7 @@ def doValue(set_pv: str, value: str, read_pv: str | type.NoneType = None) -> Non
         print('{} SP OK "{}"'.format(prefixed_set_pv, current_sp))
     if read_pv is not None:
         current_rbv = g.get_pv(prefixed_read_pv)
-        if not compareValues(current_rbv, value):
+        if not compare_values(current_rbv, value):
             if sp_ok:
                 print(
                     '{} RBV differs (but SP was OK): currrent "{}" != expected "{}"'.format(
@@ -385,8 +392,8 @@ def doValue(set_pv: str, value: str, read_pv: str | type.NoneType = None) -> Non
             print('{} RBV OK: "{}"'.format(prefixed_read_pv, current_rbv))
 
 
-def doController(config, galil: str, skips: dict) -> None:
-    cn = controllerNumber(galil)
+def do_controller(config: configparser.SectionProxy, galil: str, skips: dict) -> None:
+    cn = controller_number(galil)
     enable = config.getboolean("enable")
     address = "<None>"
     if enable:
@@ -408,28 +415,32 @@ def doController(config, galil: str, skips: dict) -> None:
         for axis_no in range(8):
             axis = chr(ord("a") + axis_no)
             if config.get(f"axis {axis} motor name") is not None:
-                doAxis(config, galil, axis, skips)
+                do_axis(config, galil, axis, skips)
     else:
         print(f"INFO: Skipping Controller {galil} (DMC{cn} MTR{cn}xx) as not enabled in seci")
 
 
 # G0 -> 01 etc.
-def controllerNumber(galil: str) -> str:
+def controller_number(galil: str) -> str:
     if galil[0] == "G":
         return "{:02d}".format(int(galil[1]) + 1)
+    else:
+        return "00"
 
 
 # construct MTRxxyy, axis 'a' -> 01
-def motorNumber(galil: str, axis: str) -> str:
-    return "MTR" + controllerNumber(galil) + "{:02d}".format(ord(axis) - ord("a") + 1)
+def motor_number(galil: str, axis: str) -> str:
+    return "MTR" + controller_number(galil) + "{:02d}".format(ord(axis) - ord("a") + 1)
 
 
 def dmc(galil: str) -> str:
-    return "DMC" + controllerNumber(galil)
+    return "DMC" + controller_number(galil)
 
 
 # return True is same else false
-def compareValues(val1: float | int, val2: float | int) -> bool:
+def compare_values(val1: PVValue | str, val2: PVValue | str) -> bool:
+    assert isinstance(val1, PVBaseValue)
+    assert isinstance(val2, PVBaseValue)
     tolerance = 0.003
     try:
         v1 = float(val1)
