b450ea262e7177788b7a66d268c04a5f7be61793
Jack Doughty <jackb.doughty@btinternet.com>
2024-10-25 17:25:45 +0100

    Covered empty file case

--- installation_and_upgrade/define_latest_genie_python.bat
+++ installation_and_upgrade/define_latest_genie_python.bat
@@ -7,22 +7,25 @@ REM	Fetch newest python build from \\isis\inst$\Kits$\CompGroup\ICP\genie_python
 REM Use genie_python_install to install it into a temporary directory
 REM Need to make sure that after every use of this bat that these variables are undefined and the temp dir is removed
 
+
 set "KITS_ICP_PATH=\\isis.cclrc.ac.uk\inst$\Kits$\CompGroup\ICP"
 set "genie_dir=%KITS_ICP_PATH%\genie_python_3"
+set "latest_build_file=%genie_dir%\LATEST_BUILD.txt"
 
-if exist "%genie_dir%\LATEST_BUILD.txt" (
+set "PYTHON_BUILD_NO="
 
+if exist "%genie_dir%\LATEST_BUILD.txt" (	
 	for /f %%i in ( %genie_dir%\LATEST_BUILD.txt ) do (
-
 		set PYTHON_BUILD_NO=%%i
-
 	)
-	
 ) else (
-
 	@echo Could not access LATEST_BUILD.txt
 	goto ERROR
-	
+)
+
+if "%PYTHON_BUILD_NO%" == "" (
+	@echo invalid LATEST_BUILD.txt
+	goto ERROR
 )
 
 if "%~1" NEQ "" (
