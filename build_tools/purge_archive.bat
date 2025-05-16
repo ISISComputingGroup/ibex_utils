60159ea3e5afdd958c55ac69ae136c747943daa4
Freddie Akeroyd <freddie.akeroyd@stfc.ac.uk>
2025-05-17 00:01:21 +0100

    Use rd instead of del

--- build_tools/purge_archive.bat
+++ build_tools/purge_archive.bat
@@ -25,7 +25,7 @@ if !errorlevel! equ 0 (
     set MYDIR=!CD!
     for /d %%i in ( !MYDIR!\Client_E4 !MYDIR!\genie_python_3 !MYDIR!\script_generator !MYDIR!\Client_E4_win11 ) do (
         if exist "%%i\branches" (
-            forfiles /p "%%i\branches" /d -60 /c "cmd /c del /q /s @path >NUL"
+            forfiles /p "%%i\branches" /d -60 /c "cmd /c rd /q /s @path >NUL"
         )
     )
 )
