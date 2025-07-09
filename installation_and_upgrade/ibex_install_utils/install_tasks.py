0a2f1109eaf8d61866030fd77a86efddb9334825
Jack Harper <jack.harper@stfc.ac.uk>
2025-07-09 13:40:57 +0100

    ruff format install_tasks

--- installation_and_upgrade/ibex_install_utils/install_tasks.py
+++ installation_and_upgrade/ibex_install_utils/install_tasks.py
@@ -3,6 +3,7 @@
 import json
 import os
 import warnings
+
 import epicscorelibs.path.pyepics  # noqa: F401
 from epics import caget
 from ibex_install_utils.ca_utils import get_machine_details_from_identifier
