4f8fed1a6f07d47154117aa2ad1f0684c45a2d2b
Tom Willemsen <tomwillemsen1995@gmail.com>
2025-07-11 15:53:24 +0100

    Update install_or_update_uv.bat

--- installation_and_upgrade/install_or_update_uv.bat
+++ installation_and_upgrade/install_or_update_uv.bat
@@ -1,6 +1,6 @@
 REM Script to either update uv or install it
 
-robocopy \\isis\inst$\Kits$\CompGroup\ICP\uv\uv-x86_64-pc-windows-msvc c:\Instrument\Apps\uv -MIR -NFL -NDL -NP -R:1 -MT -L
+robocopy \\isis\inst$\Kits$\CompGroup\ICP\uv\uv-x86_64-pc-windows-msvc\ c:\Instrument\Apps\uv\ -MIR
 set "Path=C:\Instrument\Apps\uv;%Path%"
 
 REM set the uv python executable installation directory permanently
