5a6e148e3f7f8f0505eddf12f4d7167422657479
Tom Willemsen <tom.willemsen@stfc.ac.uk>
2025-08-18 17:39:51 +0100

    ruff

--- installation_and_upgrade/ibex_install_utils/logger.py
+++ installation_and_upgrade/ibex_install_utils/logger.py
@@ -2,12 +2,13 @@ import os
 import sys
 import time
 from contextlib import contextmanager
+from typing import Generator
 
 ENABLE_LOGGING = True
 
 
 @contextmanager
-def temporarily_disable_logging():
+def temporarily_disable_logging() -> Generator[None, None, None]:
     global ENABLE_LOGGING
     previous_logging_enabled = ENABLE_LOGGING
     ENABLE_LOGGING = False
@@ -22,30 +23,30 @@ class Logger:
     Logger class used to capture output and input to a log file.
     """
 
-    def __init__(self):
-        CURRENT_DATE = time.strftime("%Y%m%d")
-        LOG_FILE = f"DEPLOY-{CURRENT_DATE}.log"
-        LOG_DIRECTORY = os.path.join("C:\\", "Instrument", "var", "logs", "deploy")
-        os.makedirs(LOG_DIRECTORY, exist_ok=True)
-        LOG_PATH = os.path.join(LOG_DIRECTORY, LOG_FILE)
+    def __init__(self) -> None:
+        current_date = time.strftime("%Y%m%d")
+        log_file = f"DEPLOY-{current_date}.log"
+        log_directory = os.path.join("C:\\", "Instrument", "var", "logs", "deploy")
+        os.makedirs(log_directory, exist_ok=True)
+        log_path = os.path.join(log_directory, log_file)
 
         self.console = sys.stdout
         self.input = sys.stdin
-        self.log = open(LOG_PATH, "a")
-        print(f"Log file is {LOG_PATH}")
+        self.log = open(log_path, "a")
+        print(f"Log file is {log_path}")
 
-    def write(self, message):
+    def write(self, message: str) -> int:
         if ENABLE_LOGGING:
             self.log.write(message)
         else:
             self.log.write("[concealed]\n")
         return self.console.write(message)
 
-    def flush(self):
+    def flush(self) -> None:
         self.console.flush()
         self.log.flush()
 
-    def readline(self):
+    def readline(self) -> str:
         text = self.input.readline()
         if ENABLE_LOGGING:
             self.log.write(text)
@@ -54,7 +55,7 @@ class Logger:
         return text
 
     @staticmethod
-    def set_up():
+    def set_up() -> None:
         logger = Logger()
         sys.stdout = logger
         sys.stderr = logger
